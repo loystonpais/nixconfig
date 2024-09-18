9e9b1de8af5aa1b4beda884b7e279ddf0b8c125f
loystonpais <loyston500@gmail.com>
2024-09-18 13:03:18 +0530

    enabled floorp browser

--- instances/roglaptop/vars.nix
+++ instances/roglaptop/vars.nix
@@ -17,7 +17,7 @@
   vars.profile.everything.enable = true;
 
   # Exclusion
-  vars.modules.browsers.floorp.enable = false;
+  vars.modules.browsers.floorp.enable = true;
   vars.modules.waydroid.enable = false;
   vars.modules.samba.enable = false;
 
