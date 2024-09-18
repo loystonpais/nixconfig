acd82802485b67b4da744a442f7b3fff85e364de
loystonpais <loyston500@gmail.com>
2024-09-18 13:12:35 +0530

    added makehuman

--- instances/roglaptop/configuration.nix
+++ instances/roglaptop/configuration.nix
@@ -1,8 +1,14 @@
-{ ... }:
+{ lib, inputs, pkgs, ... }:
 
 {
   imports = [
     ./vars.nix
+  ];
+
+
+
+  environment.systemPackages = with pkgs; [
+    makehuman
   ];
 
   system.stateVersion = "23.11"; # Did you read the comment?
