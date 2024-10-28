96598cd80186c020fd866af85dbf3352f105de28
loystonpais <loyston500@gmail.com>
2024-10-28 19:11:40 +0530

    derivation for kwin plugin

---
+++ derivations/kwin-modern-informative/default.nix
@@ -0,0 +1,26 @@
+{ lib
+, stdenv
+}:
+
+stdenv.mkDerivation {
+  pname = "kwin-modern-informative";
+  version = "unstable-2024";
+
+  src = builtins.fetchGit {
+    url = "https://framagit.org/ariasuni/kwin-windowswitcher-modern-informative.git";
+    ref = "master";
+    rev = "97976a0882d21279bdb74462ed5da08c1f4b456c";
+  };
+
+
+  installPhase = ''
+      mkdir -p $out/share/kwin/tabbox/modern_informative
+      cp -r * $out/share/kwin/tabbox/modern_informative
+  '';
+
+  meta = with lib; {
+    description = "Modern Informative task switcher for KWin";
+    license = licenses.gpl2Plus;
+    platforms = platforms.linux;
+  };
+}
