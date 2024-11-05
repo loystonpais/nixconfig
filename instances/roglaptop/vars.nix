6dbc07564a263db3afc6aa1d3510319b14324116
loystonpais <loyston500@gmail.com>
2024-11-05 11:35:25 +0530

    added ssh public key

--- instances/roglaptop/vars.nix
+++ instances/roglaptop/vars.nix
@@ -13,6 +13,10 @@
   vars.graphicsMode = "asuslinux";
   vars.bootMode = "uefi";
 
+  vars.sshPublicKeys = [
+    "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIHtN04FVSVonasScikFfltCPFJkSWa3t3z+wo+JA8GGd loyston500@gmail.com"
+  ];
+
   # Set your profile
   vars.profile.everything.enable = true;
 
