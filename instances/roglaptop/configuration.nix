41ec26f0b3e878ce8d73c9c5795577a3acde8fc7
loystonpais <loyston500@gmail.com>
2024-11-05 11:40:53 +0530

    added idk command

--- instances/roglaptop/configuration.nix
+++ instances/roglaptop/configuration.nix
@@ -1,4 +1,4 @@
-{ lib, inputs, pkgs, ... }:
+{ lib, inputs, pkgs, config, ... }:
 
 {
   imports = [
@@ -6,11 +6,17 @@
   ];
 
 
+  environment.systemPackages = [
 
-  environment.systemPackages = with pkgs; [
-    makehuman
+    (pkgs.callPackage (builtins.fetchGit {
+      url = "https://github.com/loystonpais/idk-shell-command";
+      ref = "main";
+      rev = "4090d721d4e2e42e451cb11eae7fba750ab7ca03";
+    }) { inherit pkgs; })
+ 
   ];
 
   system.stateVersion = "23.11"; # Did you read the comment?
+
 }
 
