114c2dc1c5df9fbd8becb345966cccff68966ca1
loystonpais <loyston500@gmail.com>
2024-11-06 21:34:07 +0530

    added secret 2

--- modules/secrets/default.nix
+++ modules/secrets/default.nix
@@ -1,4 +1,4 @@
-{ lib, config, inputs, ... }: 
+{ lib, config, inputs, pkgs, ... }: 
 {
   imports = [
     inputs.sops-nix.nixosModules.sops
@@ -11,12 +11,17 @@
     sops.age.keyFile = "/home/${config.vars.username}/.config/sops/age/keys.txt";
 
     sops.secrets.groq_personal_use_key.owner = config.vars.username;
-    
+    sops.secrets.gemini_api_key.owner = config.vars.username;
 
     vars.modules.secrets.environmentVariablesFromSops = {
       IDK_GROQ_API_KEY = config.sops.secrets.groq_personal_use_key;
       GROQ_API_KEY = config.sops.secrets.groq_personal_use_key;
+      GEMINI_API_KEY = config.sops.secrets.gemini_api_key;
     };
+
+    environment.systemPackages = with pkgs; [
+      sops
+    ];
 
   };  
 }
