46a6a16dd0c46f609ed9ce3c4cdc9c78e9d7e25d
loystonpais <loyston500@gmail.com>
2024-11-26 16:44:20 +0000

    nixacle: gitea: automated themes adding from assets

--- instances/nixacle/services/gitea.nix
+++ instances/nixacle/services/gitea.nix
@@ -1,9 +1,27 @@
-{ config, ... }: 
+{ config, pkgs, self, lib, ... }: 
 let 
   address = config.vars.nixacle.address;
+  
+  themes = with builtins; 
+  let
+    dir =  self.outPath + "/assets/gitea/themes/";
+    files = readDir dir;
+    cssFiles = filter (name: files.${name} == "regular" && lib.strings.hasSuffix ".css" name) (attrNames files);
+    _themes = map (file: rec { 
+      name = lib.strings.removeSuffix ".css" file; 
+      path = (dir + "/${file}");
+      verbatimCss = readFile path;  
+    }) cssFiles;
+  in
+   _themes;
 in
 
 {
+
+  systemd.tmpfiles.rules = map (theme: 
+    ''C ${config.services.gitea.customDir}/public/assets/css/theme-${theme.name}.css 0644 ${config.vars.username} gitea - ${theme.path} '' ) 
+    themes;
+
   services.gitea = {
     enable = true;
     user = config.vars.username;
@@ -14,6 +32,12 @@ in
       DOMAIN = address;
       DISABLE_REGISTRATION = true;
     };
+
+   settings.ui = {
+    	THEMES = lib.concatStringsSep "," ( [ "gitea" "arc-green" ] ++ (map (theme: theme.name) themes) );
+    	DEFAULT_THEME = "pitchblack";
+   };
+    
     appName = "Loyston's cup of tea (Coffee)";
 
     database = {
