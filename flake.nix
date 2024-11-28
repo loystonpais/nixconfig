28c1fb26ab208f9873fccfbeed69ebd17d1877be
loystonpais <loyston500@gmail.com>
2024-11-28 18:01:24 +0000

    nixconfig: channel upgrade

--- flake.nix
+++ flake.nix
@@ -61,16 +61,17 @@
 
     # Nixpkgs
     nixpkgs.url = "nixpkgs/nixos-unstable";
-    nixpkgs-24_05.url = "nixpkgs/nixos-24.05";
+    nixpkgs-24_05.url = "nixpkgs/nixos-24.05";   
+    nixpkgs-24_11.url = "nixpkgs/nixos-24.11";
 
     home-manager = {
       url = "github:nix-community/home-manager";
       inputs.nixpkgs.follows = "nixpkgs";
     };
 
-    home-manager-24_05 = {
-       url = "github:nix-community/home-manager/release-24.05";
-       inputs.nixpkgs.follows = "nixpkgs-24_05";
+    home-manager-24_11 = {
+       url = "github:nix-community/home-manager/release-24.11";
+       inputs.nixpkgs.follows = "nixpkgs-24_11";
     };
 
     nix-on-droid = {
