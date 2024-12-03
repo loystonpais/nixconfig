f8300afb88ce48b353768c0bb951cab774e7e291
loystonpais <loyston500@gmail.com>
2024-12-03 23:08:55 +0530

    secrets: added cachix auth token (loystonpais)

--- modules/secrets/default.nix
+++ modules/secrets/default.nix
@@ -15,6 +15,7 @@
     sops.secrets.github_key.owner = config.vars.username;
     sops.secrets.nixacle_gitea_db_password.owner = config.vars.username;
     sops.secrets.gitea_key.owner = config.vars.username;
+    sops.secrets.cachix_loystonpais_auth_token.owner = config.vars.username;
 
     vars.modules.secrets.environmentVariablesFromSops = {
       IDK_GROQ_API_KEY = config.sops.secrets.groq_personal_use_key;
@@ -22,6 +23,9 @@
       GEMINI_API_KEY = config.sops.secrets.gemini_api_key;
       GITHUB_KEY = config.sops.secrets.github_key;
       GITEA_KEY = config.sops.secrets.gitea_key;
+      
+      CACHIX_LOYSTONPAIS_AUTH_TOKEN = config.sops.secrets.cachix_loystonpais_auth_token;
+      CACHIX_AUTH_TOKEN = config.sops.secrets.cachix_loystonpais_auth_token;
     };
 
     environment.systemPackages = with pkgs; [
