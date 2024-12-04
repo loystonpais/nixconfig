cb53d16d413f08ba1258da41772c7a6fbd4021fb
loystonpais <loyston500@gmail.com>
2024-12-04 19:13:50 +0000

    diviner: use vps profile

--- instances/diviner/vars.nix
+++ instances/diviner/vars.nix
@@ -23,17 +23,8 @@
       "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIHtN04FVSVonasScikFfltCPFJkSWa3t3z+wo+JA8GGd loyston500@gmail.com"
     ];
 
-    vars.modules.home-manager = {
-      enable = true;
-      git.enable = true;
-      zsh.enable = true;	
-    };
 
-    vars.modules.ssh.enable = true;
-
-    vars.modules.secrets.enable = true;
-
-    vars.modules.home-manager.secrets.enable = true;
+    vars.profile.vps.enable = true;
   };
 
 }
