5a64f2be3a9cc20fee0394f9c4a4815ba69f2d35
loystonpais <loyston500@gmail.com>
2025-01-25 10:27:25 +0530

    added utils

--- utils/default.nix
+++ utils/default.nix
@@ -1,9 +1,10 @@
-with builtins; {
+with builtins; { lib ? (import <nixpkgs> {}).lib }: {
   # Imports default.nix from dirs within a given path
   # and maps them to their dir name
   importMapFromDir =
     path:
     let
-      dirs = let dirs' = readDir path; in mapA
-  ;
+      dirs = let dirs' = readDir path; in lib.attrsets.filterAttrs (n: v: n != "directory") dirs';
+    in
+      mapAttrs (n: v: import (path + ( "/" + n ))) dirs;
 }
