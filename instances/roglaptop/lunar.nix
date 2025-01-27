72306c84abe3a914e15ee643688277d8d47bf88d
loystonpais <loyston500@gmail.com>
2025-01-27 21:18:02 +0530

    roglaptop: enable wireguard client

--- instances/roglaptop/lunar.nix
+++ instances/roglaptop/lunar.nix
@@ -7,6 +7,9 @@
     "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIHtN04FVSVonasScikFfltCPFJkSWa3t3z+wo+JA8GGd loyston500@gmail.com"
   ];
 
+  # Default VPN
+  lunar.modules.vpn.wireguard.enableMode = "client";
+
   # Set your profile
   lunar.profile.everything.enable = true;
 
