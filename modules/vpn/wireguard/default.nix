b705412b7ecf706cb2b55b81b4528f25cfc96e00
loystonpais <loyston500@gmail.com>
2025-02-03 15:48:21 +0530

    hotfix: added missing port

--- modules/vpn/wireguard/default.nix
+++ modules/vpn/wireguard/default.nix
@@ -36,7 +36,7 @@
             #allowedIPs = [ "10.100.0.1" "91.108.12.0/22" ];
 
             # Set this to the server IP and port.
-            endpoint = "diviner.8300300.xyz"; # ToDo: route to endpoint not automatically configured https://wiki.archlinux.org/index.php/WireGuard#Loop_routing https://discourse.nixos.org/t/solved-minimal-firewall-setup-for-wireguard-client/7577
+            endpoint = "diviner.8300300.xyz:51820"; # ToDo: route to endpoint not automatically configured https://wiki.archlinux.org/index.php/WireGuard#Loop_routing https://discourse.nixos.org/t/solved-minimal-firewall-setup-for-wireguard-client/7577
 
             # Send keepalives every 25 seconds. Important to keep NAT tables alive.
             persistentKeepalive = 25;
