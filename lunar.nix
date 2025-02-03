f606b95ceec98fde2d1ee3bb6b8096a876b22761
loystonpais <loyston500@gmail.com>
2025-02-03 13:38:59 +0530

    lunar: added 1.1.1.1 as nameserver

--- lunar.nix
+++ lunar.nix
@@ -6,6 +6,8 @@ with lib; {
   networking.hostName = config.lunar.hostName;
   networking.networkmanager.enable = true;
   networking.firewall.enable = true;
+  networking.nameservers = [ "1.1.1.1" "8.8.8.8" "8.8.4.4" ];
+
 
   security.rtkit.enable = true;
 
