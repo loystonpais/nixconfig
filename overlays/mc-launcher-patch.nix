042d1246b1a6902cf580ef425de8daab3308c350
loystonpais <loyston500@gmail.com>
2025-02-03 16:52:06 +0530

    lunar: refactor: moved patch to packages

--- overlays/mc-launcher-patch.nix
+++ overlays/mc-launcher-patch.nix
@@ -1,34 +1,15 @@
 {
   lib,
   config,
+  inputs,
+  system,
   ...
-}: let
-  patchOfflineTrigger = ''
-    function_name="AccountListPage::on_actionAddOffline_triggered"
-    patch_file="./launcher/ui/pages/global/AccountListPage.cpp"
-
-    sed -i "/$function_name/,/^}/ s/return;/\/\/return;/" "$patch_file"
-    sed -i 's/You must add a Microsoft account that owns Minecraft before you can add an offline account/Patched Version. Just Ignore/g' "$patch_file"
-    sed -i 's/If you have lost your account you can contact Microsoft for support./Skibidi Toilet Ohio Rizz/g' "$patch_file"
-  '';
-
-  patchOwnsMinecraft = ''
-    if grep -q 'bool ownsMinecraft() const { return data.type != AccountType::Offline && data.minecraftEntitlement.ownsMinecraft; }' ./launcher/minecraft/auth/MinecraftAccount.h; then
-      sed -i 's|bool ownsMinecraft() const { return data.type != AccountType::Offline && data.minecraftEntitlement.ownsMinecraft; }|bool ownsMinecraft() const { return true; }|' ./launcher/minecraft/auth/MinecraftAccount.h
-    else
-      echo "Pattern not found."
-      exit 1
-    fi
-  '';
-in {
+}:
+{
   config = lib.mkIf config.lunar.overlays.mc-launcher-patch.enable {
     nixpkgs.overlays = [
       (final: prev: {
-        prismlauncher-unwrapped =
-          prev.prismlauncher-unwrapped.overrideAttrs
-          (oldAttrs: {
-            prePatch = patchOfflineTrigger + "\n\n" + patchOwnsMinecraft;
-          });
+        prismlauncher-unwrapped = inputs.self.packages.${system}.prismlauncher-unwrapped-crack;
       })
     ];
   };
---
+++ packages/prismlauncher-unwrapped-crack.nix
@@ -0,0 +1,21 @@
+{ pkgs ? import <nixpkgs> { } }:
+let
+  patchOfflineTrigger = ''
+    function_name="AccountListPage::on_actionAddOffline_triggered"
+    patch_file="./launcher/ui/pages/global/AccountListPage.cpp"
+
+    sed -i "/$function_name/,/^}/ s/return;/\/\/return;/" "$patch_file"
+    sed -i 's/You must add a Microsoft account that owns Minecraft before you can add an offline account/Patched Version. Just Ignore/g' "$patch_file"
+    sed -i 's/If you have lost your account you can contact Microsoft for support./Skibidi Toilet Ohio Rizz/g' "$patch_file"
+  '';
+
+  patchOwnsMinecraft = ''
+    if grep -q 'bool ownsMinecraft() const { return data.type != AccountType::Offline && data.minecraftEntitlement.ownsMinecraft; }' ./launcher/minecraft/auth/MinecraftAccount.h; then
+      sed -i 's|bool ownsMinecraft() const { return data.type != AccountType::Offline && data.minecraftEntitlement.ownsMinecraft; }|bool ownsMinecraft() const { return true; }|' ./launcher/minecraft/auth/MinecraftAccount.h
+    else
+      echo "Pattern not found."
+      exit 1
+    fi
+  '';
+in pkgs.prismlauncher-unwrapped.overrideAttrs
+(oldAttrs: { prePatch = patchOfflineTrigger + "\n\n" + patchOwnsMinecraft; })
