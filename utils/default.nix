d1636d0b1d5da3691dd367deacae7f0b1f82b983
loystonpais <loyston500@gmail.com>
2025-02-03 14:34:58 +0530

    utils: fixed importMapFromDir and added importFileMapFromDir

--- utils/default.nix
+++ utils/default.nix
@@ -1,10 +1,19 @@
-with builtins; { lib ? (import <nixpkgs> {}).lib }: {
+with builtins;
+{ lib ? (import <nixpkgs> { }).lib }: {
   # Imports default.nix from dirs within a given path
   # and maps them to their dir name
-  importMapFromDir =
-    path:
+  importMapFromDir = path:
     let
-      dirs = let dirs' = readDir path; in lib.attrsets.filterAttrs (n: v: n != "directory") dirs';
-    in
-      mapAttrs (n: v: import (path + ( "/" + n ))) dirs;
+      dirs = let dirs' = readDir path;
+      in lib.attrsets.filterAttrs (n: v: v == "directory") dirs';
+    in mapAttrs (n: v: import (path + ("/" + n))) dirs;
+
+  importFileMapFromDir = path:
+    let
+      files = let files' = readDir path;
+      in lib.attrsets.filterAttrs
+      (n: v: (v == "regular") && (lib.strings.hasSuffix ".nix" n)) files';
+    in lib.attrsets.mapAttrs' (n: v:
+      (lib.attrsets.nameValuePair (lib.strings.removeSuffix ".nix" n)
+        (import (path + ("/" + n))))) files;
 }
