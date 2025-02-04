4b97bd48bc7ee0179b282661c96f76ddebf594b0
loystonpais <loyston500@gmail.com>
2025-02-04 19:16:26 +0530

    derivations: kwin modern informative: fixed git fetching. Using pkgs.fetchgit instead of builtins counterpart

--- derivations/kwin-modern-informative/default.nix
+++ derivations/kwin-modern-informative/default.nix
@@ -1,15 +1,13 @@
-{
-  lib,
-  stdenv,
-}:
+{ lib, stdenv, fetchgit, }:
 stdenv.mkDerivation {
   pname = "kwin-modern-informative";
   version = "unstable-2024";
 
-  src = builtins.fetchGit {
-    url = "https://framagit.org/ariasuni/kwin-windowswitcher-modern-informative.git";
-    ref = "master";
+  src = fetchgit {
+    url =
+      "https://framagit.org/ariasuni/kwin-windowswitcher-modern-informative.git";
     rev = "97976a0882d21279bdb74462ed5da08c1f4b456c";
+    hash = "sha256-QStYVfPk48qRweAVoVfzp+uiQ2S9zaq2S4oevyhuLG8=";
   };
 
   installPhase = ''
