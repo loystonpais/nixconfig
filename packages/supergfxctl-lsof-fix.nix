d8682a5ce5f6cd220d20b0a34fb959239f3bff9d
loystonpais <loyston500@gmail.com>
2025-02-05 09:39:37 +0530

    packages: added supergfxctl-lsof-fix

---
+++ packages/supergfxctl-lsof-fix.nix
@@ -0,0 +1,12 @@
+{ pkgs ? import <nixpkgs> {} }:
+let
+  lsofPatch = ''
+    patch_file="./src/lib.rs"
+    if grep -q 'if !PathBuf::from("/usr/bin/lsof").exists() {' $patch_file; then
+      sed -i '/if !PathBuf::from("\/usr\/bin\/lsof").exists() {/,/}/s/^\s*return Ok(());\s*$//' $patch_file
+      sed -i 's/warn!("The lsof util is missing from your system, please ensure it is available so processes hogging Nvidia can be nuked");/warn!("Using lsof");/' $patch_file
+    else
+      echo "Patch failed."
+      exit 1
+    fi '';
+in pkgs.supergfxctl.overrideAttrs (p: { version = p.version + "-lsof-fix"; prePatch = lsofPatch; })
