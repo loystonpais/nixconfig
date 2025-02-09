4e9ca6365331164df2e0144efecdd0ca12ddb099
loystonpais <loyston500@gmail.com>
2025-02-10 01:10:05 +0530

    flake: Temporarily removed blender from gaming development environment due to build issues

--- modules/gamedev/default.nix
+++ modules/gamedev/default.nix
@@ -15,7 +15,7 @@
 
     environment.systemPackages = with pkgs; [
       godot_4
-      blender
+      /* blender */ (lib.warn "Blender has been temporarily removed from modules/gamedev due to openusd not building" pkgs.emptyFile)
       krita
       inkscape
       tiled
