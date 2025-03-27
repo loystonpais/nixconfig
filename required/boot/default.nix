fbb7e70be10b283566f50c1c43b97b47fcac36df
loystonpais <loyston500@gmail.com>
2025-03-27 20:41:40 +0530

    boot: Add warnings for unmanaged boot and graphics config

--- required/boot/default.nix
+++ required/boot/default.nix
@@ -13,5 +13,9 @@ with lib; {
     # GRUB
     boot.loader.grub.enable = true;
     boot.loader.timeout = 10;
+    warnings =
+      []
+      ++ (lib.optional (config.lunar.bootMode == "dontmanage") "Boot configuration is not managed by lunar")
+      ++ (lib.optional (config.lunar.graphicsMode == "dontmanage") "Graphics configuration is not managed by lunar");
   };
 }
