d7463ac6563aa7ccfbf0c0041de3b50d24dcfa37
loystonpais <loyston500@gmail.com>
2025-03-27 10:23:49 +0530

    home-manager: Restructure home-manager module configuration

--- modules/home-manager/default.nix
+++ modules/home-manager/default.nix
@@ -10,36 +10,40 @@
     # home-manager module is imported in the instance's default.nix
   ];
 
-  home-manager = lib.mkIf config.lunar.modules.home-manager.enable {
-    extraSpecialArgs = {
-      inherit inputs;
-      inherit system;
-      systemConfig = config;
+  config = lib.mkIf config.lunar.modules.home-manager.enable {
+    home-manager = {
+      extraSpecialArgs = {
+        inherit inputs;
+        inherit system;
+        systemConfig = config;
+      };
+      sharedModules = [inputs.plasma-manager.homeManagerModules.plasma-manager];
+      useGlobalPkgs = true;
+      useUserPackages = true;
+      users.${config.lunar.username} = {
+        imports = [
+          ./home.nix
+          ./fonts
+          ./git
+          ./hyprland
+          ./plasma
+          ./program-collection
+          ./secrets
+          ./zsh
+          ./zed
+        ];
+      };
     };
-    sharedModules = [inputs.plasma-manager.homeManagerModules.plasma-manager];
-    useGlobalPkgs = true;
-    useUserPackages = true;
-    users.${config.lunar.username}.imports = [
-      ./home.nix
-      ./fonts
-      ./git
-      ./hyprland
-      ./plasma
-      ./program-collection
-      ./secrets
-      ./zsh
-      ./zed
-    ];
-  };
 
-  lunar.modules.home-manager = lib.mkIf config.lunar.modules.home-manager.enableAllModules {
-    fonts.enable = lib.mkDefault true;
-    git.enable = lib.mkDefault true;
-    hyprland.enable = lib.mkDefault true;
-    plasma.enable = lib.mkDefault true;
-    secrets.enable = lib.mkDefault true;
-    zsh.enable = lib.mkDefault true;
-    program-collection.enable = lib.mkDefault true;
-    zed.enable = lib.mkDefault true;
+    lunar.modules.home-manager = lib.mkIf config.lunar.modules.home-manager.enableAllModules {
+      fonts.enable = lib.mkDefault true;
+      git.enable = lib.mkDefault true;
+      hyprland.enable = lib.mkDefault true;
+      plasma.enable = lib.mkDefault true;
+      secrets.enable = lib.mkDefault true;
+      zsh.enable = lib.mkDefault true;
+      program-collection.enable = lib.mkDefault true;
+      zed.enable = lib.mkDefault true;
+    };
   };
 }
