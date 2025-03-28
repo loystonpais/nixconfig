0f111e0e125a74a235ffe111c62d03684d7932a4
loystonpais <loyston500@gmail.com>
2025-03-28 21:58:47 +0530

    boot: bios: set default grub device to nodev

--- required/boot/bios.nix
+++ required/boot/bios.nix
@@ -6,6 +6,6 @@
   config = lib.mkIf (config.lunar.bootMode == "bios") {
     # BIOS
     boot.loader.grub.efiSupport = false;
-    boot.loader.grub.device = "";
+    boot.loader.grub.device = lib.mkDefault "nodev";
   };
 }
