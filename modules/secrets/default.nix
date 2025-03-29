80756785b9dd9bdf63205eb5569fcfa9e5264b30
loystonpais <loyston500@gmail.com>
2025-03-29 10:07:39 +0530

    secrets: Replace underscores with dashes in secret file names

--- modules/secrets/default.nix
+++ modules/secrets/default.nix
@@ -14,28 +14,28 @@
     sops.defaultSopsFormat = "yaml";
     sops.age.keyFile = "/home/${config.lunar.username}/.config/sops/age/keys.txt";
 
-    sops.secrets.groq_personal_use_key.owner = config.lunar.username;
-    sops.secrets.gemini_api_key.owner = config.lunar.username;
-    sops.secrets.github_key.owner = config.lunar.username;
-    sops.secrets.nixacle_gitea_db_password.owner = config.lunar.username;
-    sops.secrets.gitea_key.owner = config.lunar.username;
-    sops.secrets.cachix_loystonpais_auth_token.owner = config.lunar.username;
-    sops.secrets.ataraxy_bot_token.owner = config.lunar.username;
-    sops.secrets.ataraxy_environment_file.owner = config.lunar.username;
+    sops.secrets.groq-personal-use-key.owner = config.lunar.username;
+    sops.secrets.gemini-api-key.owner = config.lunar.username;
+    sops.secrets.github-key.owner = config.lunar.username;
+    sops.secrets.nixacle-gitea-db-password.owner = config.lunar.username;
+    sops.secrets.gitea-key.owner = config.lunar.username;
+    sops.secrets.cachix-loystonpais-auth-token.owner = config.lunar.username;
+    sops.secrets.ataraxy-bot-token.owner = config.lunar.username;
+    sops.secrets.ataraxy-environment-file.owner = config.lunar.username;
     sops.secrets.wireguard-server-common-private-key.owner = config.lunar.username;
-    sops.secrets."wireguard-client-${config.lunar.hostName}-private-key" = lib.mkIf (config.lunar.modules.vpn.wireguard.enableMode == "client" && config.lunar.modules.vpn.wireguard.clientPrivateKeyInSops) { owner = config.lunar.username;};
+    sops.secrets."wireguard-client-${config.lunar.hostName}-private-key" = lib.mkIf (config.lunar.modules.vpn.wireguard.enableMode == "client" && config.lunar.modules.vpn.wireguard.clientPrivateKeyInSops) {owner = config.lunar.username;};
 
     lunar.modules.secrets.environmentVariablesFromSops = {
-      IDK_GROQ_API_KEY = config.sops.secrets.groq_personal_use_key;
-      GROQ_API_KEY = config.sops.secrets.groq_personal_use_key;
-      GEMINI_API_KEY = config.sops.secrets.gemini_api_key;
-      GITHUB_KEY = config.sops.secrets.github_key;
-      GITEA_KEY = config.sops.secrets.gitea_key;
+      IDK_GROQ_API_KEY = config.sops.secrets.groq-personal-use-key;
+      GROQ_API_KEY = config.sops.secrets.groq-personal-use-key;
+      GEMINI_API_KEY = config.sops.secrets.gemini-api-key;
+      GITHUB_KEY = config.sops.secrets.github-key;
+      GITEA_KEY = config.sops.secrets.gitea-key;
 
-      CACHIX_LOYSTONPAIS_AUTH_TOKEN = config.sops.secrets.cachix_loystonpais_auth_token;
-      CACHIX_AUTH_TOKEN = config.sops.secrets.cachix_loystonpais_auth_token;
+      CACHIX_LOYSTONPAIS_AUTH_TOKEN = config.sops.secrets.cachix-loystonpais-auth-token;
+      CACHIX_AUTH_TOKEN = config.sops.secrets.cachix-loystonpais-auth-token;
 
-      ATARAXY_BOT_TOKEN = config.sops.secrets.ataraxy_bot_token;
+      ATARAXY_BOT_TOKEN = config.sops.secrets.ataraxy-bot-token;
     };
 
     sops.secrets.auto-resume-builder = {
