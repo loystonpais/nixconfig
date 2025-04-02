4d869acbea60190a947c7d75dd7905b3d5fc5a0a
loystonpais <loyston500@gmail.com>
2025-04-02 12:07:46 +0530

    secrets: Add shell aliases to copy secrets to clipboard

--- modules/home-manager/secrets/default.nix
+++ modules/home-manager/secrets/default.nix
@@ -1,12 +1,16 @@
 {
   systemConfig,
   lib,
+  pkgs,
   ...
-}: {
+}:
+with builtins; let
+  vars = systemConfig.lunar.modules.secrets.environmentVariablesFromSops;
+in {
   config = lib.mkIf (systemConfig.lunar.modules.secrets.enable && systemConfig.lunar.modules.home-manager.secrets.enable) {
-    programs.zsh.initExtra = with builtins; let
-      lunar = systemConfig.lunar.modules.secrets.environmentVariablesFromSops;
-    in
-      concatStringsSep "\n" (map (name: ''export '${name}'="$(cat ${lunar.${name}.path})"'') (attrNames lunar));
+    programs.zsh.initExtra =
+      concatStringsSep "\n" (map (name: ''export '${name}'="$(cat ${vars.${name}.path})"'') (attrNames vars));
+    programs.zsh.shellAliases =
+      lib.mkIf (systemConfig.lunar.modules.graphics.enable) (lib.attrsets.mapAttrs' (name: attrs: lib.attrsets.nameValuePair ("copy-" + name) "cat ${attrs.path} | ${pkgs.xclip}/bin/xclip -selection c") vars);
   };
 }
