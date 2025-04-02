adb5af71f80bd47f2b6fd7619c28525c102b2d4b
loystonpais <loyston500@gmail.com>
2025-04-02 12:06:28 +0530

    secrets: Add SOPS secret for ftp.sh DNS update URL

--- modules/secrets/default.nix
+++ modules/secrets/default.nix
@@ -24,6 +24,7 @@
     sops.secrets.ataraxy-environment-file.owner = config.lunar.username;
     sops.secrets.wireguard-server-common-private-key.owner = config.lunar.username;
     sops.secrets."wireguard-client-${config.lunar.hostName}-private-key" = lib.mkIf (config.lunar.modules.vpn.wireguard.enableMode == "client" && config.lunar.modules.vpn.wireguard.clientPrivateKeyInSops) {owner = config.lunar.username;};
+    sops.secrets.loy-ftp-sh-dns-update-url.owner = config.lunar.username;
 
     lunar.modules.secrets.environmentVariablesFromSops = {
       IDK_GROQ_API_KEY = config.sops.secrets.groq-personal-use-key;
