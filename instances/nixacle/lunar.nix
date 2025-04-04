7235976d2332339a990f06348ff5bef4531cef3a
loystonpais <loyston500@gmail.com>
2025-04-04 22:11:52 +0530

    nixacle, diviner: set rclone mounts

--- instances/nixacle/lunar.nix
+++ instances/nixacle/lunar.nix
@@ -28,5 +28,14 @@
     ];
 
     lunar.profile.vps.enable = true;
+
+    lunar.modules.rclone = {
+      dropbox500.enable = true;
+      pcloud500.enable = true;
+      # Turned of box because it's buggy
+      # box500.enable = true;
+      koofr500.enable = true;
+      unions.mega.enable = true;
+    };
   };
 }
