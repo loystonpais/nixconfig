2094a59ce898df44b48f7433dc01906f44d2e304
loystonpais <loyston500@gmail.com>
2025-04-13 08:08:45 +0530

    audio: Enable JACK support and add user to audio groups

--- modules/audio/default.nix
+++ modules/audio/default.nix
@@ -1,17 +1,24 @@
-{ config, lib, ... }: {
+{
+  config,
+  lib,
+  ...
+}: {
   config = lib.mkIf config.lunar.modules.audio.enable {
     hardware.pulseaudio.enable = false;
+    security.rtkit.enable = true;
     services.pipewire = {
       enable = true;
       alsa.enable = true;
       alsa.support32Bit = true;
       pulse.enable = true;
-      # If you want to use JACK applications, uncomment this
-      #jack.enable = true;
+
+      jack.enable = lib.mkForce true;
 
       # use the example session manager (no others are packaged yet so this is enabled by default,
       # no need to redefine it in your config for now)
       #media-session.enable = true;
     };
+    
+    users.extraUsers.${config.lunar.username}.extraGroups = ["jackaudio"];
   };
 }
