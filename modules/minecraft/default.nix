1706a2b8950afc3ce09e26d70c1f5921a1187772
loystonpais <loyston500@gmail.com>
2025-04-23 18:45:30 +0530

    packages: add ninjabrainbot to minecraft module

--- modules/minecraft/default.nix
+++ modules/minecraft/default.nix
@@ -3,12 +3,15 @@
   config,
   lib,
   pkgs,
+  inputs,
+  system,
   ...
 }: {
   config = lib.mkIf config.lunar.modules.minecraft.enable {
     environment.systemPackages = with pkgs; [
       prismlauncher
       jdk21
+      inputs.self.packages.${system}.ninjabrainbot
     ];
   };
 }
---
+++ packages/ninjabrainbot.nix
@@ -0,0 +1,52 @@
+# source: https://github.com/MonkieeBoi/nix-config/blob/ba497457e886071ab715b5ec569b5a2dc748db4f/pkgs/ninb.nix
+{
+  lib,
+  stdenv,
+  fetchurl,
+  makeWrapper,
+  jre,
+  libxkbcommon,
+  xorg,
+}:
+stdenv.mkDerivation rec {
+  name = "ninjabrainbot";
+  version = "1.5.1";
+
+  src = fetchurl {
+    url = "https://github.com/Ninjabrain1/Ninjabrain-Bot/releases/download/${version}/Ninjabrain-Bot-${version}.jar";
+    hash = "sha256-Rxu9A2EiTr69fLBUImRv+RLC2LmosawIDyDPIaRcrdw=";
+  };
+
+  dontUnpack = true;
+
+  nativeBuildInputs = [
+    makeWrapper
+  ];
+
+  installPhase = ''
+    runHook preInstall
+
+    install -Dm644 ${src} $out/share/java/${name}-${version}.jar
+
+    makeWrapper ${jre}/bin/java $out/bin/${name} \
+      --add-flags "-jar $out/share/java/${name}-${version}.jar" \
+      --prefix LD_LIBRARY_PATH : ${
+      lib.makeLibraryPath [
+        libxkbcommon
+        xorg.libX11
+        xorg.libXt
+        xorg.libXtst
+        xorg.libXinerama
+        xorg.libxcb
+      ]
+    }
+
+    runHook postInstall
+  '';
+
+  meta = {
+    homepage = "https://github.com/Ninjabrain1/Ninjabrain-Bot";
+    description = "Accurate stronghold calculator for Minecraft speedrunning.";
+    platforms = lib.platforms.linux;
+  };
+}
