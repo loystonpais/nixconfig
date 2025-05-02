bd7aaec640c6b324171dfc60bef0cf07d0c7bf6e
loystonpais <loyston500@gmail.com>
2025-05-02 10:28:55 +0530

    zsh: renamed initExtra to initContent

--- modules/home-manager/secrets/default.nix
+++ modules/home-manager/secrets/default.nix
@@ -8,7 +8,7 @@ with builtins; let
   vars = systemConfig.lunar.modules.secrets.environmentVariablesFromSops;
 in {
   config = lib.mkIf (systemConfig.lunar.modules.secrets.enable && systemConfig.lunar.modules.home-manager.secrets.enable) {
-    programs.zsh.initExtra =
+    programs.zsh.initContent =
       concatStringsSep "\n" (map (name: ''export '${name}'="$(cat ${vars.${name}.path})"'') (attrNames vars));
     programs.zsh.shellAliases =
       lib.mkIf (systemConfig.lunar.modules.graphics.enable) (lib.attrsets.mapAttrs' (name: attrs: lib.attrsets.nameValuePair ("copy-" + name) "cat ${attrs.path} | ${pkgs.xclip}/bin/xclip -selection c") vars);
