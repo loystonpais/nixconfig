f7c15932b04f651659b64d501e35b5e76b7a6aea
loystonpais <loyston500@gmail.com>
2025-05-14 00:34:55 +0530

    roglaptop: Move lunar related config to lunar.nix

--- instances/roglaptop/lunar.nix
+++ instances/roglaptop/lunar.nix
@@ -7,6 +7,18 @@
     "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIHtN04FVSVonasScikFfltCPFJkSWa3t3z+wo+JA8GGd loyston500@gmail.com"
   ];
 
+  lunar.modules.virtual-machine.cgroupDevicesById = [
+    "usb-Razer_Razer_DeathAdder_Essential-event-mouse"
+    "usb-Usb_KeyBoard_Usb_KeyBoard-event-kbd"
+  ];
+
+  lunar.modules.android.adbDevices = {
+    vili = {
+      ip = "192.168.43.2";
+      port = 5555;
+    };
+  };
+
   # Set your profile
   lunar.profile.everything.enable = true;
 
@@ -14,4 +26,10 @@
   lunar.modules.browsers.zen.enable = true;
   lunar.modules.waydroid.enable = false;
   lunar.modules.samba.enable = false;
+
+  # Specialisations
+  lunar.specialisations = {
+    enable = true;
+    productive.enable = true;
+  };
 }
