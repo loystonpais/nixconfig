591a61025a4388fb5d5d2f0a38b2268d37b43c80
loystonpais <loyston500@gmail.com>
2025-05-19 22:02:50 +0530

    secrets: Add copy-ENVVARs as packages to make their usage shell agnostic

--- modules/home-manager/secrets/default.nix
+++ modules/home-manager/secrets/default.nix
@@ -10,7 +10,15 @@ in {
   config = lib.mkIf (systemConfig.lunar.modules.secrets.enable && systemConfig.lunar.modules.home-manager.secrets.enable) {
     programs.zsh.initContent =
       concatStringsSep "\n" (map (name: ''export '${name}'="$(cat ${vars.${name}.path})"'') (attrNames vars));
-    programs.zsh.shellAliases =
-      lib.mkIf (systemConfig.lunar.modules.graphics.enable) (lib.attrsets.mapAttrs' (name: attrs: lib.attrsets.nameValuePair ("copy-" + name) "cat ${attrs.path} | ${pkgs.xclip}/bin/xclip -selection c") vars);
+
+    home.packages = lib.mkMerge [
+      (
+        lib.mkIf
+        systemConfig.lunar.modules.graphics.enable
+        (attrValues (mapAttrs (name: attrs:
+          pkgs.writeShellScriptBin ("copy-" + name) "cat '${attrs.path}' | ${pkgs.xclip}/bin/xclip -selection c")
+        vars))
+      )
+    ];
   };
 }
