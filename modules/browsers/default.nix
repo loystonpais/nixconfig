0f484a1856d39f2b9b73145edc9fb3171d7a44b2
loystonpais <loyston500@gmail.com>
2025-05-20 22:03:57 +0530

    browsers: switched back to firefox from firefox-esr

--- modules/browsers/default.nix
+++ modules/browsers/default.nix
@@ -5,8 +5,7 @@ with lib; {
   config = mkIf config.lunar.modules.browsers.enable {
     programs.firefox = {
       enable = true;
-      package = pkgs.firefox-esr;
-      preferences = { "widget.use-xdg-desktop-portal.file-picker" = 1; };
+      preferences = {"widget.use-xdg-desktop-portal.file-picker" = 1;};
     };
     lunar.modules.browsers.zen.enable = true;
   };
