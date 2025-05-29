ca471c1dbff6af2634f1537d49bd676bfa86471f
loystonpais <loyston500@gmail.com>
2025-05-29 20:25:33 +0530

    packages: add brisk

---
+++ packages/brisk.nix
@@ -0,0 +1,87 @@
+{
+  lib,
+  stdenv,
+  fetchurl,
+  makeWrapper,
+  gcc,
+  gtk3,
+  gdk-pixbuf,
+  pango,
+  cairo,
+  at-spi2-core,
+  keybinder3,
+  libdbusmenu,
+  libayatana-indicator,
+  libayatana-appindicator,
+  ayatana-ido,
+  harfbuzz,
+  glib,
+  libepoxy,
+  fontconfig,
+}:
+stdenv.mkDerivation rec {
+  pname = "brisk";
+  version = "2.2.5";
+
+  src = fetchurl {
+    url = "https://github.com/BrisklyDev/brisk/releases/download/v${version}/Brisk-v${version}-linux-x86_64.tar.xz";
+    hash = "sha256-YGUzyyFiQqCzelIzFK1W2nD19PVxAZNAGly9ptwtOic=";
+  };
+
+  dontUnpack = true;
+
+  nativeBuildInputs = [
+    makeWrapper
+  ];
+
+  installPhase = ''
+    runHook preInstall
+
+    DIR="$out/share/${pname}"
+
+    mkdir -p $DIR
+    mkdir -p $out/share/applications/
+    mkdir -p $out/bin
+
+    tar -xvf $src -C $DIR
+
+    rm -rf $DIR/updater
+
+    cp $DIR/brisk.desktop $out/share/applications/
+    substituteInPlace $out/share/applications/brisk.desktop \
+      --replace "Exec=brisk" "Exec=$out/bin/${pname}" \
+      --replace "Icon=data/flutter_assets/assets/icons/logo.png" "Icon=$DIR/data/flutter_assets/assets/icons/logo.png"
+
+    chmod +x $DIR/brisk
+    makeWrapper $DIR/brisk $out/bin/${pname} \
+      --chdir  $DIR \
+      --prefix LD_LIBRARY_PATH : ${
+      lib.makeLibraryPath [
+        gcc
+        gtk3
+        gdk-pixbuf
+        pango
+        cairo
+        at-spi2-core
+        keybinder3
+        libdbusmenu
+        libayatana-indicator
+        libayatana-appindicator
+        ayatana-ido
+        stdenv.cc.cc.lib
+        harfbuzz
+        glib
+        libepoxy
+        fontconfig
+      ]
+    }
+
+    runHook postInstall
+  '';
+
+  meta = {
+    homepage = "https://github.com/BrisklyDev/brisk";
+    description = "Ultra-fast, modern download manager for desktop ";
+    platforms = lib.platforms.linux;
+  };
+}
