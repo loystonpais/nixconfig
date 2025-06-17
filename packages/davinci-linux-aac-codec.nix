6ff6b79c201960005db6cbb09cefe4866b9d4f4c
loystonpais <loyston500@gmail.com>
2025-06-17 12:41:53 +0530

    packages: added davinci-linux-acc-coded (davinci resolve plugin)

--- packages/davinci-linux-aac-codec.nix
+++ packages/davinci-linux-aac-codec.nix
@@ -1,26 +1,55 @@
 {
   stdenv,
-  fetchurl,
+  fetchgit,
+  cmake,
+  clang,
   ffmpeg,
+  libcxx,
 }:
-stdenv.mkDerivation rec {
+stdenv.mkDerivation {
   pname = "davinci-linux-aac-codec";
   version = "1.0.0";
 
-  src = fetchurl {
-    url = "https://github.com/Toxblh/davinci-linux-aac-codec/releases/download/v${version}/aac_encoder_plugin-linux-bundle.tar.gz";
-    sha256 = "sha256-KQpqUp2zveVGLD84Aurn3MvapLaETJYVoMcNLA+Fg3o=";
+  src = fetchgit {
+    url = "https://github.com/Toxblh/davinci-linux-aac-codec";
+    rev = "bca3dad197ed00bfc232acde79a5fe032f240f7a";
+    hash = "sha256-sm0KF2be0jYTnOYVGQciWJ9IzBlWqWhs7H9oNk3G3TI=";
   };
 
-  buildInputs = [ffmpeg];
+  nativeBuildInputs = [
+    cmake
+    clang
+  ];
+
+  buildInputs = [
+    ffmpeg
+    libcxx
+  ];
+
+  # Custom option which is used by davinci resolve
+  runtimeDependencies = [
+    ffmpeg
+  ];
 
   dontUnpack = true;
+  dontConfigure = true;
+
+  buildPhase = ''
+    cp -r --no-preserve=mode,ownership,timestamps $src $TMPDIR/src
+    cd src
+    make
+  '';
 
   installPhase = ''
-    mkdir -p $out/opt/resolve/IOPlugins
-    tar -xvf $src -C $TMPDIR
-    cp -r $TMPDIR/aac_encoder_plugin.dvcp.bundle $out/opt/resolve/IOPlugins/
-    chmod -R 755 $out/opt/resolve/IOPlugins
+    BUNDLE_DIR="aac_encoder_plugin.dvcp.bundle/Contents/Linux-x86-64"
+    PLUGIN_NAME="aac_encoder_plugin.dvcp"
+
+    mkdir -p $out/IOPlugins/
+    mkdir -p $out/IOPlugins/$BUNDLE_DIR
+
+    cp $TMPDIR/src/bin/$PLUGIN_NAME $out/IOPlugins/$BUNDLE_DIR
+    chmod -R 755 $out/IOPlugins
+    chmod +x $out/IOPlugins/$BUNDLE_DIR/$PLUGIN_NAME
   '';
 
   meta = {
