f7ea1470adf3aeae55488845070fd124590cc898
loystonpais <loyston500@gmail.com>
2025-06-17 13:06:09 +0530

    waydroid: add waydroid script and wl-clipboard

--- modules/waydroid/default.nix
+++ modules/waydroid/default.nix
@@ -1,9 +1,16 @@
 {
   config,
   lib,
+  pkgs,
+  inputs,
+  system,
   ...
 }: {
   config = lib.mkIf config.lunar.modules.waydroid.enable {
     virtualisation.waydroid.enable = true;
+    environment.systemPackages = with pkgs; [
+      wl-clipboard
+      inputs.nur.legacyPackages.${system}.repos.ataraxiasjel.waydroid-script
+    ];
   };
 }
