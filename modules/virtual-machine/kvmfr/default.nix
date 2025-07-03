34e92f6e1f2b4f99a04c95b42bfb47f5b250c99f
loystonpais <loyston500@gmail.com>
2025-07-03 20:45:32 +0530

    virtual-machine: add kvmfr sub module

---
+++ modules/virtual-machine/kvmfr/default.nix
@@ -0,0 +1,55 @@
+{
+  lib,
+  pkgs,
+  config,
+  inputs,
+  system,
+  ...
+}:
+with lib; let
+  cfg = config.virtualisation.kvmfr;
+in {
+  options.virtualisation.kvmfr = {
+    enable = mkEnableOption "Kvmfr";
+
+    shm = {
+      enable = mkEnableOption "shm";
+
+      size = mkOption {
+        type = types.int;
+        default = "128";
+        description = "Size of the shared memory device in megabytes.";
+      };
+      user = mkOption {
+        type = types.str;
+        default = "root";
+        description = "Owner of the shared memory device.";
+      };
+      group = mkOption {
+        type = types.str;
+        default = "root";
+        description = "Group of the shared memory device.";
+      };
+      mode = mkOption {
+        type = types.str;
+        default = "0600";
+        description = "Mode of the shared memory device.";
+      };
+    };
+  };
+
+  config = mkIf cfg.enable {
+    boot.extraModulePackages = with config.boot.kernelPackages; [
+      (inputs.self.packages.${system}.kvmfr {inherit kernel;})
+    ];
+    boot.initrd.kernelModules = ["kvmfr"];
+
+    boot.kernelParams = optionals cfg.shm.enable [
+      "kvmfr.static_size_mb=${toString cfg.shm.size}"
+    ];
+
+    services.udev.extraRules = optionals cfg.shm.enable ''
+      SUBSYSTEM=="kvmfr", OWNER="${cfg.shm.user}", GROUP="${cfg.shm.group}", MODE="${cfg.shm.mode}"
+    '';
+  };
+}
