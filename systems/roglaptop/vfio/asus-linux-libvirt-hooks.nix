ed1926735b508a87c5b4eb94dde519faa570cb4e
loystonpais <loyston500@gmail.com>
2025-10-18 19:07:13 +0530

    roglaptop: improve asus linux libvirt hooks impl

--- systems/roglaptop/vfio/asus-linux-libvirt-hooks.nix
+++ systems/roglaptop/vfio/asus-linux-libvirt-hooks.nix
@@ -44,158 +44,137 @@
         SYS_TOTAL_CPUS=0-15
       '';
 
-      hooksDir = pkgs.linkFarm "qemu-hooks-dir" [
-        {
-          name = "win11/prepare/begin/10-asusd-vfio.sh";
-          path = pkgs.writeShellScript "10-asusd-vfio.sh" ''
-            source "${vmVarsConf}"
+      hooksDir = pkgs.linkFarm "qemu-hooks-dir" {
+        "win11/prepare/begin/10-asusd-vfio.sh" = pkgs.writeShellScript "10-asusd-vfio.sh" ''
+          source "${vmVarsConf}"
 
-            supergfxctl -m Vfio
+          supergfxctl -m Vfio
 
-            echo "Graphics mode set!"
-            sleep 1
-          '';
-        }
+          echo "Graphics mode set!"
+          sleep 1
+        '';
 
-        {
-          name = "win11/prepare/begin/20-reserve-hugepages.sh";
-          path = pkgs.writeShellScript "20-reserve-hugepages.sh" ''
-            source "${vmVarsConf}"
+        "win11/prepare/begin/20-reserve-hugepages.sh" = pkgs.writeShellScript "20-reserve-hugepages.sh" ''
+          source "${vmVarsConf}"
 
-            HUGEPAGES="$(($VM_MEMORY/$(($(grep Hugepagesize /proc/meminfo | awk '{print $2}')))))"
+          HUGEPAGES="$(($VM_MEMORY/$(($(grep Hugepagesize /proc/meminfo | awk '{print $2}')))))"
 
-            echo "Allocating hugepages at 2048 KiB per page..."
-            echo $HUGEPAGES > /sys/kernel/mm/hugepages/hugepages-2048kB/nr_hugepages
-            ALLOC_PAGES=$(cat /proc/sys/vm/nr_hugepages)
+          echo "Allocating hugepages at 2048 KiB per page..."
+          echo $HUGEPAGES > /sys/kernel/mm/hugepages/hugepages-2048kB/nr_hugepages
+          ALLOC_PAGES=$(cat /proc/sys/vm/nr_hugepages)
 
-            ## If successful, notify user
-            if [ "$ALLOC_PAGES" -eq "$HUGEPAGES" ]
-            then
-                echo "Successfully allocated $ALLOC_PAGES / $HUGEPAGES pages!"
-            fi
+          ## If successful, notify user
+          if [ "$ALLOC_PAGES" -eq "$HUGEPAGES" ]
+          then
+              echo "Successfully allocated $ALLOC_PAGES / $HUGEPAGES pages!"
+          fi
 
-            ## Drop caches to free up memory for hugepages if not successful
-            if [ "$ALLOC_PAGES" -ne "$HUGEPAGES" ]
-            then
-                echo 3 > /proc/sys/vm/drop_caches
-            fi
+          ## Drop caches to free up memory for hugepages if not successful
+          if [ "$ALLOC_PAGES" -ne "$HUGEPAGES" ]
+          then
+              echo 3 > /proc/sys/vm/drop_caches
+          fi
 
-            TRIES=0
-            while (( $ALLOC_PAGES != $HUGEPAGES && $TRIES < 1000 ))
-            do
-                ## Defrag RAM then try to allocate pages again
-                echo 1 > /proc/sys/vm/compact_memory
-                echo $HUGEPAGES > /sys/kernel/mm/hugepages/hugepages-2048kB/nr_hugepages
-                ALLOC_PAGES=$(cat /proc/sys/vm/nr_hugepages)
-                ## If successful, notify user
-                echo "Successfully allocated $ALLOC_PAGES / $HUGEPAGES pages!"
-                let TRIES+=1
-            done
+          TRIES=0
+          while (( $ALLOC_PAGES != $HUGEPAGES && $TRIES < 1000 ))
+          do
+              ## Defrag RAM then try to allocate pages again
+              echo 1 > /proc/sys/vm/compact_memory
+              echo $HUGEPAGES > /sys/kernel/mm/hugepages/hugepages-2048kB/nr_hugepages
+              ALLOC_PAGES=$(cat /proc/sys/vm/nr_hugepages)
+              ## If successful, notify user
+              echo "Successfully allocated $ALLOC_PAGES / $HUGEPAGES pages!"
+              let TRIES+=1
+          done
 
-            if [ "$ALLOC_PAGES" -ne "$HUGEPAGES" ]
-            then
-                echo "Not able to allocate all hugepages. Reverting..."
-                echo 0 > /proc/sys/vm/nr_hugepages
-                exit 1
-            fi
+          if [ "$ALLOC_PAGES" -ne "$HUGEPAGES" ]
+          then
+              echo "Not able to allocate all hugepages. Reverting..."
+              echo 0 > /proc/sys/vm/nr_hugepages
+              exit 1
+          fi
 
-            sleep 1
-          '';
-        }
+          sleep 1
+        '';
 
-        {
-          name = "win11/prepare/begin/30-set-governor.sh";
-          path = pkgs.writeShellScript "30-set-governor.sh" ''
-            source "${vmVarsConf}"
+        "win11/prepare/begin/30-set-governor.sh" = pkgs.writeShellScript "30-set-governor.sh" ''
+          source "${vmVarsConf}"
 
 
-            ## Set CPU governor to mode indicated by variable
-            CPU_COUNT=0
-            for file in /sys/devices/system/cpu/cpu*/cpufreq/scaling_governor
-            do
-                echo $VM_ON_GOVERNOR > $file;
-                echo "CPU $CPU_COUNT governor: $VM_ON_GOVERNOR";
-                let CPU_COUNT+=1
-            done
+          ## Set CPU governor to mode indicated by variable
+          CPU_COUNT=0
+          for file in /sys/devices/system/cpu/cpu*/cpufreq/scaling_governor
+          do
+              echo $VM_ON_GOVERNOR > $file;
+              echo "CPU $CPU_COUNT governor: $VM_ON_GOVERNOR";
+              let CPU_COUNT+=1
+          done
 
-            ## Set system power profile to performance
-            powerprofilesctl set $VM_ON_PWRPROFILE
+          ## Set system power profile to performance
+          powerprofilesctl set $VM_ON_PWRPROFILE
 
-            sleep 1
-          '';
-        }
+          sleep 1
+        '';
 
-        {
-          name = "win11/prepare/begin/40-isolate-cpus.sh";
-          path = pkgs.writeShellScript "40-isolate-cpus.sh" ''
-            source "${vmVarsConf}"
+        "win11/prepare/begin/40-isolate-cpus.sh" = pkgs.writeShellScript "40-isolate-cpus.sh" ''
+          source "${vmVarsConf}"
 
-            ## Isolate CPU cores as per set variable
-            systemctl set-property --runtime -- user.slice AllowedCPUs=$VM_ISOLATED_CPUS
-            systemctl set-property --runtime -- system.slice AllowedCPUs=$VM_ISOLATED_CPUS
-            systemctl set-property --runtime -- init.scope AllowedCPUs=$VM_ISOLATED_CPUS
+          ## Isolate CPU cores as per set variable
+          systemctl set-property --runtime -- user.slice AllowedCPUs=$VM_ISOLATED_CPUS
+          systemctl set-property --runtime -- system.slice AllowedCPUs=$VM_ISOLATED_CPUS
+          systemctl set-property --runtime -- init.scope AllowedCPUs=$VM_ISOLATED_CPUS
 
-            sleep 1
-          '';
-        }
+          sleep 1
+        '';
 
-        {
-          name = "win11/release/end/10-release-hugepages.sh";
-          path = pkgs.writeShellScript "10-release-hugepages.sh" ''
-            source "${vmVarsConf}"
+        "win11/release/end/10-release-hugepages.sh" = pkgs.writeShellScript "10-release-hugepages.sh" ''
+          source "${vmVarsConf}"
 
-            ## Remove Hugepages
-            echo "Releasing hugepage memory back to the host..."
-            echo 0 > /sys/kernel/mm/hugepages/hugepages-2048kB/nr_hugepages
+          ## Remove Hugepages
+          echo "Releasing hugepage memory back to the host..."
+          echo 0 > /sys/kernel/mm/hugepages/hugepages-2048kB/nr_hugepages
 
-            ## Advise if successful
-            ALLOC_PAGES=$(cat /proc/sys/vm/nr_hugepages)
+          ## Advise if successful
+          ALLOC_PAGES=$(cat /proc/sys/vm/nr_hugepages)
 
-            if [ "$ALLOC_PAGES" -eq 0 ]
-            then
-                echo "Memory successfully released!"
-            fi
+          if [ "$ALLOC_PAGES" -eq 0 ]
+          then
+              echo "Memory successfully released!"
+          fi
 
-            sleep 1
+          sleep 1
 
-          '';
-        }
+        '';
 
-        {
-          name = "win11/release/end/20-return-cpus.sh";
-          path = pkgs.writeShellScript "20-return-cpus.sh" ''
-            source "${vmVarsConf}"
+        "win11/release/end/20-return-cpus.sh" = pkgs.writeShellScript "20-return-cpus.sh" ''
+          source "${vmVarsConf}"
 
-            ## Return CPU cores as per set variable
-            systemctl set-property --runtime -- user.slice AllowedCPUs=$SYS_TOTAL_CPUS
-            systemctl set-property --runtime -- system.slice AllowedCPUs=$SYS_TOTAL_CPUS
-            systemctl set-property --runtime -- init.scope AllowedCPUs=$SYS_TOTAL_CPUS
+          ## Return CPU cores as per set variable
+          systemctl set-property --runtime -- user.slice AllowedCPUs=$SYS_TOTAL_CPUS
+          systemctl set-property --runtime -- system.slice AllowedCPUs=$SYS_TOTAL_CPUS
+          systemctl set-property --runtime -- init.scope AllowedCPUs=$SYS_TOTAL_CPUS
 
-            sleep 1
-          '';
-        }
+          sleep 1
+        '';
 
-        {
-          name = "win11/release/end/30-restore-governor.sh";
-          path = pkgs.writeShellScript "30-restore-governor.sh" ''
-            source "${vmVarsConf}"
+        "win11/release/end/30-restore-governor.sh" = pkgs.writeShellScript "30-restore-governor.sh" ''
+          source "${vmVarsConf}"
 
-            ## Restore CPU governor to mode indicated by variable
-            CPU_COUNT=0
-            for file in /sys/devices/system/cpu/cpu*/cpufreq/scaling_governor
-            do
-                echo $VM_OFF_GOVERNOR > $file;
-                echo "CPU $CPU_COUNT governor: $VM_OFF_GOVERNOR";
-                let CPU_COUNT+=1
-            done
+          ## Restore CPU governor to mode indicated by variable
+          CPU_COUNT=0
+          for file in /sys/devices/system/cpu/cpu*/cpufreq/scaling_governor
+          do
+              echo $VM_OFF_GOVERNOR > $file;
+              echo "CPU $CPU_COUNT governor: $VM_OFF_GOVERNOR";
+              let CPU_COUNT+=1
+          done
 
-            ## Restore system power profile to default
-            powerprofilesctl set $VM_OFF_PWRPROFILE
+          ## Restore system power profile to default
+          powerprofilesctl set $VM_OFF_PWRPROFILE
 
-            sleep 1
-          '';
-        }
-      ];
+          sleep 1
+        '';
+      };
     in
       pkgs.writeShellScript "qemu-nix" ''
         GUEST_NAME="$1"
