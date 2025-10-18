b92b9a593cec01c5be20d68ed1b2030fa61ee588
loystonpais <loyston500@gmail.com>
2025-10-18 19:06:21 +0530

    roglaptop: update & clean up config; move hardware config to extra-hardware;

--- systems/roglaptop/configuration.nix
+++ systems/roglaptop/configuration.nix
@@ -8,90 +8,9 @@
 }: {
   imports = [
     ./lunar.nix
+    ./extra-hardware.nix
     ./vfio
   ];
-
-  services.pipewire.extraConfig.pipewire."80-combined" = {
-    "context.modules" = [
-      {
-        name = "libpipewire-module-combine-stream";
-        args = {
-          "combine.mode" = "sink";
-          "node.name" = "systemoutput+hdmioutput";
-          "node.description" = "Both Laptop speakers and HDMI/Monitor speakers (systemoutput+hdmioutput)";
-          "node.nick" = "systemoutput+hdmioutput";
-          "combine.latency-compensate" = true;
-
-          "combine.volume-sync" = true;
-          "node.autoconnect" = true;
-
-          "combine.props" = {
-            "audio.position" = ["FL" "FR"];
-          };
-          "stream.rules" = [
-            {
-              matches = [
-                {
-                  # "media.class" = "Audio/Sink";
-                  "node.name" = "alsa_output.pci-0000_06_00.6.analog-stereo";
-                }
-              ];
-              actions = {
-                create-stream = {
-                  "combine.audio.position" = ["FL" "FR"];
-                  "audio.position" = ["FL" "FR"];
-                };
-              };
-            }
-
-            {
-              matches = [
-                {
-                  # "media.class" = "Audio/Sink";
-                  "node.name" = "alsa_output.pci-0000_01_00.1.hdmi-stereo";
-                }
-              ];
-              actions = {
-                create-stream = {
-                  "combine.audio.position" = ["FL" "FR"];
-                  "audio.position" = ["FL" "FR"];
-                };
-              };
-            }
-          ];
-        };
-      }
-    ];
-  };
-
-  services.pipewire.wireplumber.extraConfig."70-naming" = let
-    rule = name: newNick: newDescription: {
-      matches = [{"node.name" = name;}];
-      actions = {
-        update-props = {
-          "node.nick" = newNick;
-          "node.description" = newDescription;
-        };
-      };
-    };
-  in {
-    "monitor.alsa.rules" = [
-      (rule
-        "alsa_output.pci-0000_06_00.6.analog-stereo"
-        "systemoutput"
-        "Laptop inbuilt speakers (systemoutput)")
-
-      (rule "alsa_output.pci-0000_01_00.1.hdmi-stereo"
-        "hdmioutput"
-        "HDMI/Monitor Speakers (hdmioutput)")
-
-      # input ones
-      (rule
-        "alsa_input.pci-0000_06_00.6.analog-stereo"
-        "systeminput"
-        "Laptop inbuilt microphone (systeminput)")
-    ];
-  };
 
   # Prevents ollama from redownloading...
   environment.variables.OLLAMA_NOPRUNE = lib.mkDefault "true";
@@ -100,36 +19,56 @@
     pkgs.cachix
     pkgs.gcc
     pkgs.rclone
-
     inputs.self.packages.${system}.nautilus-scripts
-
     pkgs.devenv
-
     pkgs.nix-update
-
     pkgs.nautilus
+    inputs.self.packages.${system}.iso2god-rs
+    pkgs.chromium
+    # pkgs.ungoogled-chromium
   ];
 
-  networking.dhcpcd.extraConfig = ''
-    interface wlo1
-    metric 99
+  programs.starship = {
+    enable = true;
+  };
 
-    interface enp4s0
-    metric 98
-  '';
+  programs.zsh.enable = lib.mkForce false;
 
-  networking.interfaces.wlo1.useDHCP = lib.mkDefault true;
-  networking.interfaces.enp4s0.useDHCP = lib.mkDefault true;
+  programs.xonsh = {
+    enable = true;
+    extraPackages = ps:
+      with ps; [
+        numpy
+        xonsh.xontribs.xontrib-vox
+        xonsh.xontribs.xonsh-direnv
+        # coconut
+        requests
+      ];
+  };
 
-  networking.firewall.enable = lib.mkForce false;
+  users.users.${config.lunar.username} = {
+    shell = lib.mkForce pkgs.xonsh;
+  };
 
-  hardware.i2c.enable = true;
+  programs.direnv = {
+    enable = true;
+    enableXonshIntegration = true;
+    enableBashIntegration = true;
+    enableZshIntegration = true;
+  };
+
+  programs.zoxide = {
+    enable = true;
+    enableXonshIntegration = true;
+  };
 
   lunar.modules.misc.supergfxd-lsof-overlay.enable = false;
 
   nixpkgs.overlays = [
     inputs.self.overlays.lunar
   ];
+
+  networking.firewall.enable = lib.mkForce false;
 
   # NOTE: move this to hardware-configuration.nix once the uid issue is fixed
   fileSystems."/mnt/windows" = {
@@ -155,64 +94,59 @@
     # interfaces.enp7s0f3u1u2.useDHCP = true;
   };
 
-  services.displayManager.defaultSession = lib.mkForce "niri";
+  # boot.kernel.sysctl."net.ipv4.ip_forward" = 1;
 
-  boot.kernel.sysctl."net.ipv4.ip_forward" = 1;
+  # systemd.services.auractl-wallpaper = let
+  #   scheme =
+  #     if lib.isAttrs config.stylix.base16Scheme
+  #     then builtins.toFile config.stylix.base16Scheme
+  #     else config.stylix.base16Scheme;
+  #   aura = "breathe";
+  #   offset = 10;
+  # in {
+  #   enable = true;
+  #   path = [pkgs.asusctl pkgs.yq];
+  #   script = ''
+  #     palette=($(yq ".palette[]" -r '${scheme}' | cut -c 2-))
 
-  systemd.services.auractl-wallpaper = let
-    #wallpaper = config.home-manager.users.${config.lunar.username}.programs.plasma.kscreenlocker.appearance.wallpaper
-    #or config.stylix.image;
-    wallpaper = config.lunar.wallpaper;
-    scheme = config.stylix.base16Scheme;
-    aura = "breathe";
-    offset = 10;
-  in {
-    enable = true;
-    path = [pkgs.asusctl pkgs.yq];
-    script = ''
-      palette=($(yq ".palette[]" -r '${scheme}' | cut -c 2-))
+  #     # zone starts from 1
+  #     for zone in {1..4}; do
+  #       col=''${palette[(( $zone + ${builtins.toString offset} ))]}
+  #       asusctl aura "${aura}" -c "$col" -z $zone;
+  #       echo "Applied ${aura} $col to zone $zone";
+  #     done;
+  #   '';
+  #   serviceConfig = {
+  #     Type = "oneshot";
+  #   };
+  #   after = ["asusd.service"];
+  #   wantedBy = ["multi-user.target"];
+  # };
 
-      # zone starts from 1
-      for zone in {1..4}; do
-        col=''${palette[(( $zone + ${builtins.toString offset} ))]}
-        asusctl aura "${aura}" -c "$col" -z $zone;
-        echo "Applied ${aura} $col to zone $zone";
-      done;
-    '';
-    serviceConfig = {
-      Type = "oneshot";
+  specialisation.plasma = {
+    configuration = {
+      config = {
+        lunar.wallpaper = "${inputs.self}/assets/wallpapers/green-leaves.jpg";
+        lunar.modules.niri.enable = lib.mkForce false;
+        lunar.modules.waybar.enable = lib.mkForce false;
+
+        lunar.modules.plasma.enable = lib.mkForce true;
+
+        services.displayManager.defaultSession = lib.mkForce "plasma";
+
+        systemd.services = {
+          "delete-hm-backups-${config.lunar.username}" = {
+            script = ''
+              find /home/${config.lunar.username}/.config -name "*.nixbak" -delete
+            '';
+            before = ["home-manager-${config.lunar.username}.service"];
+          };
+        };
+      };
     };
-    after = ["asusd.service"];
-    wantedBy = ["multi-user.target"];
-  };
-
-  hardware.nvidia.prime = {
-    offload.enable = true;
-    offload.enableOffloadCmd = true;
-    amdgpuBusId = "PCI:06:00:0";
-    nvidiaBusId = "PCI:01:00:0";
   };
 
   boot.tmp.cleanOnBoot = true;
-
-  home-manager.users.${config.lunar.username}.imports = [
-    {
-      home.file.".local/share/PrismLauncher/instances/FSG/minecraft/fsg" = let
-        pkg = inputs.self.packages.${system}.zig-seed-glitchless.override {
-          zsgConfig = {
-            enable_terrain_checker = true;
-          };
-        };
-      in {
-        source = "${pkg}/share/${pkg.pname}";
-        recursive = true;
-        force = true;
-      };
-
-      programs.obsidian.enable = true;
-      programs.chromium.enable = lib.mkForce false;
-    }
-  ];
 
   system.stateVersion = "23.11"; # Did you read the comment?
 }
---
+++ systems/roglaptop/extra-hardware.nix
@@ -0,0 +1,110 @@
+{
+  config,
+  lib,
+  pkgs,
+  ...
+}: {
+  config = {
+    hardware.nvidia.prime = {
+      offload.enable = true;
+      offload.enableOffloadCmd = true;
+      amdgpuBusId = "PCI:06:00:0";
+      nvidiaBusId = "PCI:01:00:0";
+    };
+
+    networking.dhcpcd.extraConfig = ''
+      interface wlo1
+      metric 99
+
+      interface enp4s0
+      metric 98
+    '';
+
+    networking.interfaces.wlo1.useDHCP = lib.mkDefault true;
+    networking.interfaces.enp4s0.useDHCP = lib.mkDefault true;
+
+    hardware.i2c.enable = true;
+
+    services.pipewire.extraConfig.pipewire."80-combined" = {
+      "context.modules" = [
+        {
+          name = "libpipewire-module-combine-stream";
+          args = {
+            "combine.mode" = "sink";
+            "node.name" = "systemoutput+hdmioutput";
+            "node.description" = "Both Laptop speakers and HDMI/Monitor speakers (systemoutput+hdmioutput)";
+            "node.nick" = "systemoutput+hdmioutput";
+            "combine.latency-compensate" = true;
+
+            "combine.volume-sync" = true;
+            "node.autoconnect" = true;
+
+            "combine.props" = {
+              "audio.position" = ["FL" "FR"];
+            };
+            "stream.rules" = [
+              {
+                matches = [
+                  {
+                    # "media.class" = "Audio/Sink";
+                    "node.name" = "alsa_output.pci-0000_06_00.6.analog-stereo";
+                  }
+                ];
+                actions = {
+                  create-stream = {
+                    "combine.audio.position" = ["FL" "FR"];
+                    "audio.position" = ["FL" "FR"];
+                  };
+                };
+              }
+
+              {
+                matches = [
+                  {
+                    # "media.class" = "Audio/Sink";
+                    "node.name" = "alsa_output.pci-0000_01_00.1.hdmi-stereo";
+                  }
+                ];
+                actions = {
+                  create-stream = {
+                    "combine.audio.position" = ["FL" "FR"];
+                    "audio.position" = ["FL" "FR"];
+                  };
+                };
+              }
+            ];
+          };
+        }
+      ];
+    };
+
+    services.pipewire.wireplumber.extraConfig."70-naming" = let
+      rule = name: newNick: newDescription: {
+        matches = [{"node.name" = name;}];
+        actions = {
+          update-props = {
+            "node.nick" = newNick;
+            "node.description" = newDescription;
+          };
+        };
+      };
+    in {
+      "monitor.alsa.rules" = [
+        (rule
+          "alsa_output.pci-0000_06_00.6.analog-stereo"
+          "systemoutput"
+          "Laptop inbuilt speakers (systemoutput)")
+
+        (rule "alsa_output.pci-0000_01_00.1.hdmi-stereo"
+          "hdmioutput"
+          "HDMI/Monitor Speakers (hdmioutput)")
+
+        # input ones
+        (rule
+          "alsa_input.pci-0000_06_00.6.analog-stereo"
+          "systeminput"
+          "Laptop inbuilt microphone (systeminput)")
+      ];
+    };
+  };
+}
