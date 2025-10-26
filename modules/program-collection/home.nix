cb71824d95df116fd56db498f29473d09f70f08a
loystonpais <loyston500@gmail.com>
2025-10-26 22:44:00 +0530

    program-collection: add rclone, cachix. nix-update

--- modules/program-collection/home.nix
+++ modules/program-collection/home.nix
@@ -24,6 +24,7 @@
       bat
       xonsh
       rsync
+      rclone
       wl-clipboard
 
       # Nix tooling
@@ -31,6 +32,9 @@
       nix-du
       nix-health
       nix-heuristic-gc
+
+      cachix
+      nix-update
     ];
   };
 }
