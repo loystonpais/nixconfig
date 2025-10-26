a49906c6ab81b43b98dc92193b60cb29cc2709d6
loystonpais <loyston500@gmail.com>
2025-10-26 22:38:46 +0530

    xonsh: enable xonsh and add extra packages

--- modules/xonsh/default.nix
+++ modules/xonsh/default.nix
@@ -10,6 +10,20 @@
 
   config = lib.mkIf config.lunar.modules.xonsh.enable (lib.mkMerge [
     {
+      programs.xonsh = {
+        enable = true;
+        extraPackages = ps:
+          with ps; [
+            numpy
+            xonsh.xontribs.xontrib-vox
+            xonsh.xontribs.xonsh-direnv
+            # coconut #? Doesn't work because of python verison mismatch
+            requests
+          ];
+      };
+    }
+
+    {
       lunar.modules.xonsh.home.enable = lib.mkDefault true;
       home-manager.users.${config.lunar.username}.imports = [
         ./home.nix
