c4a9a2c1317b5fb3d3e5171bf7f51153c01fcb19
loystonpais <loyston500@gmail.com>
2025-11-19 23:52:10 +0530

    zsh: remove ohmyzsh in favour of starship

--- modules/zsh/home.nix
+++ modules/zsh/home.nix
@@ -53,29 +53,6 @@
         share = true;
         path = "${config.home.homeDirectory}/.zsh_history";
       };
-
-      oh-my-zsh = {
-        enable = true;
-        plugins = ["git" "direnv"];
-        theme = "robbyrussell";
-      };
-
-      #     plugins = [
-      #       {
-      #         name = "zsh-nix-shell";
-      #         file = "nix-shell.plugin.zsh";
-      #         src = pkgs.fetchFromGitHub {
-      #           owner = "chisui";
-      #           repo = "zsh-nix-shell";
-      #           rev = "v0.8.0";
-      #           sha256 = "1lzrn0n4fxfcgg65v0qhnj7wnybybqzs4adz7xsrkgmcsr0ii8b7";
-      #         };
-      #       }
-      #     ];
-
-      #     initExtra = ''
-      #       #prompt_nix_shell_setup
-      #     '';
     };
   };
 }
