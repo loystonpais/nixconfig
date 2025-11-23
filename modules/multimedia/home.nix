59a20e7886b6ec82bc717a20ddc35df524914f1b
loystonpais <loyston500@gmail.com>
2025-11-23 22:37:43 +0530

    secrets: update gemini api key

---
+++ modules/multimedia/home.nix
@@ -0,0 +1,35 @@
+{
+  osConfig,
+  config,
+  lib,
+  pkgs,
+  ...
+}: {
+  config = lib.mkIf osConfig.lunar.modules.multimedia.enable (lib.mkMerge [
+    # MPV
+    {
+      programs.mpv = {
+        enable = true;
+
+        package = (
+          pkgs.mpv-unwrapped.wrapper {
+            scripts = with pkgs.mpvScripts; [
+              uosc
+              sponsorblock
+            ];
+
+            mpv = pkgs.mpv-unwrapped.override {
+              waylandSupport = true;
+            };
+          }
+        );
+
+        config = {
+          profile = "high-quality";
+          ytdl-format = "bestvideo+bestaudio";
+          cache-default = 4000000;
+        };
+      };
+    }
+  ]);
+}
