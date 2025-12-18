03f61b956d6dce0ae619e7a0ebac2fa187974297
loystonpais <loyston500@gmail.com>
2025-12-18 17:30:08 +0530

    font: update font pkg

--- modules/fonts/home.nix
+++ modules/fonts/home.nix
@@ -10,7 +10,7 @@
     home.packages = with pkgs; [
       noto-fonts
       noto-fonts-cjk-sans
-      noto-fonts-emoji
+      noto-fonts-color-emoji
       nerd-fonts.symbols-only
       fira-code
       fira-code-symbols
