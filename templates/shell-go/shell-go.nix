b341a768041e79bf8f102baee31e5835ca979aeb
loystonpais <loyston500@gmail.com>
2025-12-30 13:50:46 +0530

    templates: update go template

--- templates/shell-go/shell-go.nix
+++ templates/shell-go/shell-go.nix
@@ -3,11 +3,11 @@ with builtins;
 with pkgs;
   mkShell rec {
     name = "Go Shell";
-    buildInputs = [
+    packges = [
       go
       gopls
     ];
     shellHook = ''
-      echo -e "\033[32;1;3;5m${name} initialized with packages ${pkgs.lib.concatStringsSep ", " (builtins.map (x: x.name) buildInputs)}\033[0m"
+      echo -e "\033[32;1;3;5m${name} initialized with packages ${pkgs.lib.concatStringsSep ", " (builtins.map (x: x.name) packges)}\033[0m"
     '';
   }
