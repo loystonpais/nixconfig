2c89d9e9d958a85ad2a69bb2e7d28072cd0b6b4c
loystonpais <loyston500@gmail.com>
2026-01-22 12:11:47 +0530

    android: get rid of adb.enable

--- modules/android/default.nix
+++ modules/android/default.nix
@@ -1,6 +1,7 @@
 {
   config,
   lib,
+  pkgs,
   ...
 }: {
   imports = [
@@ -8,10 +9,9 @@
   ];
 
   config = lib.mkIf config.lunar.modules.android.enable {
-
-    programs.adb.enable = true;
+    environment.systemPackages = [pkgs.android-tools];
     # Prevents using sudo
-    users.users.${config.lunar.username}.extraGroups = [ "adbusers" ];
+    users.users.${config.lunar.username}.extraGroups = ["adbusers"];
 
     lunar.modules.android.scrcpy.enable = lib.mkDefault true;
   };
