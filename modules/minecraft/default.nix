b8369655ce4938506727101ac2c45061b11bdcc2
loystonpais <loyston500@gmail.com>
2026-02-25 09:59:17 +0530

    minecraft: remove minecraft-cursorfix, prismlauncher crack

--- modules/minecraft/default.nix
+++ modules/minecraft/default.nix
@@ -32,7 +32,6 @@
       environment.etc."java-collection/jdk21".source = pkgs.jdk21;
 
       environment.etc."glfw-collection/glfw3".source = pkgs.glfw3;
-      environment.etc."glfw-collection/glfw3-minecraft-cursorfix".source = inputs.self.packages.${system}.glfw3-minecraft-cursorfix;
       environment.etc."glfw-collection/glfw3-waywall".source = inputs.self.packages.${system}.glfw3-waywall;
 
       environment.systemPackages = with pkgs; [
@@ -44,19 +43,5 @@
         inputs.self.packages.${system}.zig-seed-glitchless
       ];
     }
-
-    (
-      lib.mkIf (config.lunar.modules.minecraft.cracked && config.lunar.expensiveBuilds) {
-        nixpkgs.overlays = [
-          # NOTE: put this overlay in the overlays folder
-          # inputs.self.overlays.prismlauncher-crack
-          (
-            final: prev: {
-              prismlauncher-unwrapped = inputs.self.packages.${system}.prismlauncher-unwrapped-crack;
-            }
-          )
-        ];
-      }
-    )
   ]);
 }
