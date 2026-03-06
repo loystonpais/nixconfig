02b33e693837e93aa37e02c7ea5b1d3a0151b857
loystonpais <loyston500@gmail.com>
2026-03-06 12:47:58 +0530

    nixcraft: config update

--- modules/minecraft/nixcraft/default.nix
+++ modules/minecraft/nixcraft/default.nix
@@ -4,7 +4,9 @@
   inputs,
   ...
 }: {
-  imports = [inputs.nixcraft.nixosModules.default];
+  imports = [
+    # inputs.nixcraft.nixosModules.default
+  ];
 
   options.lunar.modules.nixcraft = {
     enable = lib.mkEnableOption "nixcraft";
@@ -17,33 +19,6 @@
       home-manager.users.${config.lunar.username}.imports = [
         ./home.nix
       ];
-    }
-
-    {
-      nixcraft = {
-        enable = true;
-        server = {
-          instances = {
-            superb-server = {
-              version = "1.21.1";
-              agreeToEula = true;
-              enable = true;
-              java.memory = 2000;
-              serverProperties = {
-                level-seed = "6969";
-                online-mode = false;
-                bug-report-link = null;
-              };
-              # servers can be run as systemd user services
-              # service name is set as nixcraft-server-<name>.service
-              service = {
-                enable = true;
-                autoStart = false;
-              };
-            };
-          };
-        };
-      };
     }
   ]);
 }
