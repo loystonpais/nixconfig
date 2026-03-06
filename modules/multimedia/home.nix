622ae44dce726fccc0f504255268442ae61d211c
loystonpais <loyston500@gmail.com>
2026-03-06 12:43:28 +0530

    multimedia: remove mpv config

--- modules/multimedia/home.nix
+++ modules/multimedia/home.nix
@@ -10,25 +10,6 @@
     {
       programs.mpv = {
         enable = true;
-
-        package = (
-          pkgs.mpv-unwrapped.wrapper {
-            scripts = with pkgs.mpvScripts; [
-              uosc
-              sponsorblock
-            ];
-
-            mpv = pkgs.mpv-unwrapped.override {
-              waylandSupport = true;
-            };
-          }
-        );
-
-        config = {
-          profile = "high-quality";
-          ytdl-format = "bestvideo+bestaudio";
-          cache-default = 4000000;
-        };
       };
     }
   ]);
