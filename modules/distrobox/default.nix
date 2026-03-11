df1be5532cfa6755acba79f5d787aa7b9ae02f49
loystonpais <loyston500@gmail.com>
2026-03-11 12:10:59 +0530

    distrobox: move podman to separate module

--- modules/distrobox/default.nix
+++ modules/distrobox/default.nix
@@ -5,31 +5,12 @@
   ...
 }: {
   config = lib.mkIf config.lunar.modules.distrobox.enable {
-    environment.systemPackages = with pkgs; [distrobox];
-
-    virtualisation.podman = {
-      enable = true;
-      dockerCompat = true;
-    };
+    environment.systemPackages = with pkgs; [
+      distrobox
+    ];
 
     environment.etc."distrobox/distrobox.conf".text = ''
       container_additional_volumes="/nix/store:/nix/store:ro /etc/profiles/per-user:/etc/profiles/per-user:ro /etc/static/profiles/per-user:/etc/static/profiles/per-user:ro"
     '';
-
-    users.users.${config.lunar.username} = {
-      extraGroups = ["podman"];
-      subGidRanges = [
-        {
-          count = 65536;
-          startGid = 1000;
-        }
-      ];
-      subUidRanges = [
-        {
-          count = 65536;
-          startUid = 1000;
-        }
-      ];
-    };
   };
 }
