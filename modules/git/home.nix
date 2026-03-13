1cbd17b968d2445b5b75466140713434201616f5
loystonpais <loyston500@gmail.com>
2026-03-13 18:34:48 +0530

    git: adapt new config

--- modules/git/home.nix
+++ modules/git/home.nix
@@ -7,12 +7,14 @@
   config = lib.mkIf osConfig.lunar.modules.git.home.enable {
     programs.git = {
       enable = true;
-      userName = osConfig.lunar.github.username;
-      userEmail = osConfig.lunar.email;
-      aliases = {
-        pu = "push";
-        ch = "checkout";
-        cm = "commit";
+      settings = {
+        user.name = osConfig.lunar.github.username;
+        user.email = osConfig.lunar.email;
+        alias = {
+          pu = "push";
+          ch = "checkout";
+          cm = "commit";
+        };
       };
     };
   };
