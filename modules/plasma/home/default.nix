4d8b5f2ecb3801be7cd7fa7e1ec2d1e7927f9b4e
loystonpais <loyston500@gmail.com>
2026-03-13 18:40:55 +0530

    plasma: attempt to fix gtk theme

--- modules/plasma/home/default.nix
+++ modules/plasma/home/default.nix
@@ -32,10 +32,30 @@
       };
 
       ## GTK Theminng
+      gtk.iconTheme.package = pkgs.whitesur-icon-theme;
+      gtk.iconTheme.name = "WhiteSur-dark";
+
+      gtk.theme.package = pkgs.whitesur-gtk-theme;
+      gtk.theme.name = "WhiteSur-Dark";
+
+      gtk.gtk3.extraConfig = {
+        gtk-dialogs-use-header = false;
+        gtk-cursor-theme-size = 0;
+        gtk-toolbar-style = "GTK_TOOLBAR_BOTH_HORIZ";
+        gtk-toolbar-icon-size = "GTK_ICON_SIZE_LARGE_TOOLBAR";
+        gtk-button-images = 0;
+        gtk-menu-images = 0;
+        gtk-enable-event-sounds = 0;
+        gtk-enable-input-feedback-sounds = 1;
+        gtk-xft-antialias = 1;
+        gtk-xft-hinting = 1;
+        gtk-xft-hintstyle = "hintslight";
+        gtk-xft-rgba = "none";
+      };
 
       # makes some gtk apps (not libadwaita) apps pick up
       # custom theme
-      home.sessionVariables.GTK_THEME = "WhiteSur-Dark";
+      # home.sessionVariables.GTK_THEME = "WhiteSur-Dark";
 
       # Fixes theme polarity (light/dark) between several gtk4?
       # apps such as gparted and libadwaita apps such as bottles
