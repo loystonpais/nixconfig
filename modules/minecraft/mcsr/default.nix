ccb65262bd8545e5f663d388b5350c531b096e7a
loystonpais <loyston500@gmail.com>
2026-03-29 19:46:11 +0530

    minecraft: mcsr: fix config guard

--- modules/minecraft/mcsr/default.nix
+++ modules/minecraft/mcsr/default.nix
@@ -11,29 +11,29 @@ in {
     enable = lib.mkEnableOption "minecraft mcsr";
   };
 
-  config = {
-    lunar.modules.minecraft.enable = true;
+  config = lib.mkIf config.lunar.modules.minecraft.mcsr.enable (lib.mkMerge [
+    {
+      home-manager.users.${config.lunar.username}.imports = [
+        ({
+          config,
+          lib,
+          osConfig,
+          ...
+        }: {
+          home.file."${waywallCfgDir}/init.lua" = {
+            source =
+              config.lib.file.mkOutOfStoreSymlink
+              "${osConfig.lunar.flakePath}/modules/minecraft/mcsr/waywall/init.lua";
+          };
+        })
+      ];
 
-    home-manager.users.${config.lunar.username}.imports = [
-      ({
-        config,
-        lib,
-        osConfig,
-        ...
-      }: {
-        home.file."${waywallCfgDir}/init.lua" = {
-          source =
-            config.lib.file.mkOutOfStoreSymlink
-            "${osConfig.lunar.flakePath}/modules/minecraft/mcsr/waywall/init.lua";
-        };
-      })
-    ];
+      environment.systemPackages = with pkgs; [
+        waywall
+      ];
 
-    environment.systemPackages = with pkgs; [
-      waywall
-    ];
-
-    # Xwayland should be running for waywall to open ninb
-    programs.xwayland.enable = true;
-  };
+      # Xwayland should be running for waywall to open ninb
+      programs.xwayland.enable = true;
+    }
+  ]);
 }
