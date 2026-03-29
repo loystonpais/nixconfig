098273fd72e01ea13c5c35f946a25943cef344cc
loystonpais <loyston500@gmail.com>
2026-03-29 17:54:07 +0530

    xonsh: fix external module

--- modules/xonsh/default.nix
+++ modules/xonsh/default.nix
@@ -8,28 +8,35 @@
     home.enable = lib.mkEnableOption "xonsh home-manager";
   };
 
-  config = lib.mkIf config.lunar.modules.xonsh.enable (lib.mkMerge [
+  config = lib.mkMerge [
     {
-      programs.xonsh = {
-        enable = true;
-        extraPackages = ps:
-          with ps; [
-            numpy
-            xonsh.xontribs.xontrib-vox
-            xonsh.xontribs.xonsh-direnv
-            # coconut #? Doesn't work because of python verison mismatch
-            requests
-            groq
-          ];
-      };
-    }
-
-    {
-      lunar.modules.xonsh.home.enable = lib.mkDefault true;
       home-manager.users.${config.lunar.username}.imports = [
-        ./home.nix
         ./external-home-module.nix
       ];
     }
-  ]);
+
+    (lib.mkIf config.lunar.modules.xonsh.enable (lib.mkMerge [
+      {
+        programs.xonsh = {
+          enable = true;
+          extraPackages = ps:
+            with ps; [
+              numpy
+              xonsh.xontribs.xontrib-vox
+              xonsh.xontribs.xonsh-direnv
+              # coconut #? Doesn't work because of python verison mismatch
+              requests
+              groq
+            ];
+        };
+      }
+
+      {
+        lunar.modules.xonsh.home.enable = lib.mkDefault true;
+        home-manager.users.${config.lunar.username}.imports = [
+          ./home.nix
+        ];
+      }
+    ]))
+  ];
 }
