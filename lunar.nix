b4ac9f24535118f12ad8a062c1ac1bc6c8cd922f
loystonpais <loyston500@gmail.com>
2026-04-03 13:29:06 +0530

    flake: add cache priority

--- lunar.nix
+++ lunar.nix
@@ -49,14 +49,12 @@ with lib; {
 
   nix.settings = {
     substituters = lib.mkAfter [
-      "https://loystonpais.cachix.org"
-      "https://nix-community.cachix.org"
-      # "https://cache.garnix.io" /* Garnix is very slow and buggy for some reason */
+      "https://loystonpais.cachix.org?priority=10"
+      "https://nix-community.cachix.org?priority=5"
     ];
     trusted-public-keys = [
       "loystonpais.cachix.org-1:lclfaBitH51Lw9WwBxQ4bbesdt7c01JlFbKoSZ0PMLc="
       "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
-      # "cache.garnix.io:CTFPyKSLcx5RMJKfLo5EEPUObbA78b0YQ2DTCJXqr9g="
     ];
     experimental-features = ["nix-command" "flakes"];
     trusted-users = ["root" config.lunar.username];
