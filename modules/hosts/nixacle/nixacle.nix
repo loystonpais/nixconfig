2b36adea828afb73a84b06a0b5789a8bf1a3b6ad
loystonpais <loyston500@gmail.com>
2026-04-20 07:01:30 +0000

    nixacle: temporarily enable nginx

--- modules/hosts/nixacle/nixacle.nix
+++ modules/hosts/nixacle/nixacle.nix
@@ -14,8 +14,36 @@
       ];
 
       networking.firewall = {
-        enable = lib.mkForce false;
-        allowedTCPPorts = [25565 1888];
+        enable = true;
+        allowedTCPPorts = [25565 80 443];
+      };
+
+      security.acme.certs."loy.ftp.sh".listenHTTP = lib.mkForce null;
+
+      services.nginx = {
+        enable = true;
+        recommendedGzipSettings = true;
+        recommendedOptimisation = true;
+
+        virtualHosts."loy.ftp.sh" = {
+          enableACME = true;
+          forceSSL = true;
+
+          locations = {
+            "/" = {
+              proxyPass = "http://localhost:5000";
+              extraConfig = ''
+                client_max_body_size 512M;
+                proxy_set_header Connection $http_connection;
+                proxy_set_header Upgrade $http_upgrade;
+                proxy_set_header Host $host;
+                proxy_set_header X-Real-IP $remote_addr;
+                proxy_set_header X-Forwarded-For $proxy_add_x_forwarded_for;
+                proxy_set_header X-Forwarded-Proto $scheme;
+              '';
+            };
+          };
+        };
       };
     };
   };
