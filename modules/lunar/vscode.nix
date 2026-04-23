28d333aae721bdbb516518feb9532705fb0ad7b6
loystonpais <loyston500@gmail.com>
2026-04-23 11:24:15 +0000

    vscode: fix profiles

--- modules/lunar/vscode.nix
+++ modules/lunar/vscode.nix
@@ -3,7 +3,83 @@
   inputs,
   ...
 }: {
-  lunar.vscode = {
+  lunar.vscode = let
+    mkCommonSettings = {
+      pkgs,
+      lib,
+      ...
+    }: {
+      "update.mode" = "none";
+      "continue.telemetryEnabled" = false;
+      "diffEditor.ignoreTrimWhitespace" = false;
+      "extensions.autoUpdate" = false;
+      "explorer.confirmDragAndDrop" = false;
+      "git.enableSmartCommit" = true;
+      "redhat.telemetry.enabled" = false;
+      "security.workspace.trust.untrustedFiles" = "open";
+      "telemetry.enableCrashReporter" = false;
+      "telemetry.enableTelemetry" = false;
+      "telemetry.telemetryLevel" = "off";
+      "workbench.sideBar.location" = "right";
+      "terminal.integrated.copyOnSelection" = true;
+      "terminal.integrated.cursorBlinking" = true;
+      "terminal.integrated.cursorStyle" = "line";
+      "terminal.integrated.persistentSessionScrollback" = 1000;
+      "terminal.integrated.ignoreBracketedPasteMode" = false;
+      "terminal.integrated.rightClickBehavior" = "paste";
+      "terminal.integrated.scrollback" = 3000;
+      "window.closeOnFileDelete" = true;
+      "workbench.colorTheme" = "Dark Modern";
+      "workbench.iconTheme" = "vscode-icons";
+      "workbench.preferredDarkColorTheme" = "Dark Modern";
+      "workbench.statusBar.visible" = true;
+      "editor.formatOnSave" = true;
+      "editor.wordWrap" = "on";
+      "editor.wordWrapColumn" = 120;
+      "editor.fontLigatures" = true;
+      "editor.tabSize" = 2;
+
+      "nix.enableLanguageServer" = true;
+      "nix.serverPath" = lib.getExe pkgs.nixd;
+      "nix.formatterPath" = lib.getExe pkgs.alejandra;
+    };
+
+    mkCommonExtensions = {pkgs, ...}:
+      (with pkgs.vscode-extensions; [
+        # Essential Nix support
+        bbenoist.nix
+        jnoortheen.nix-ide
+        kamadorueda.alejandra
+        jeff-hykin.better-nix-syntax
+
+        # Essential workflow
+        mkhl.direnv
+        ms-vscode-remote.remote-ssh
+        christian-kohler.path-intellisense
+      ])
+      ++ (with pkgs.vscode-marketplace; [
+        # Essential UI/UX
+        pkief.material-icon-theme
+        eamodio.gitlens
+        mhutchie.git-graph
+        vivaxy.vscode-conventional-commits
+        aaron-bond.better-comments
+        editorconfig.editorconfig
+        oderwat.indent-rainbow
+        shardulm94.trailing-spaces
+        kisstkondoros.vscode-gutter-preview
+
+        # Workflow
+        datakurre.devenv
+
+        # Templating
+        karunamurti.tera
+
+        # Others
+        kdl-org.kdl
+        legale.dts-formatter
+      ]);
+  in {
     nixos = {pkgs, ...}: {
       environment.systemPackages = [pkgs.vscode];
 
@@ -18,72 +94,15 @@
       lib,
       ...
     }: let
-      # Common settings shared across all profiles
-      commonSettings = {
-        "update.mode" = "none";
-        "continue.telemetryEnabled" = false;
-        "diffEditor.ignoreTrimWhitespace" = false;
-        "extensions.autoUpdate" = false;
-        "explorer.confirmDragAndDrop" = false;
-        "git.enableSmartCommit" = true;
-        "redhat.telemetry.enabled" = false;
-        "security.workspace.trust.untrustedFiles" = "open";
-        "telemetry.enableCrashReporter" = false;
-        "telemetry.enableTelemetry" = false;
-        "telemetry.telemetryLevel" = "off";
-        "workbench.sideBar.location" = "right";
-        "terminal.integrated.copyOnSelection" = true;
-        "terminal.integrated.cursorBlinking" = true;
-        "terminal.integrated.cursorStyle" = "line";
-        "terminal.integrated.persistentSessionScrollback" = 1000;
-        "terminal.integrated.ignoreBracketedPasteMode" = false;
-        "terminal.integrated.rightClickBehavior" = "paste";
-        "terminal.integrated.scrollback" = 3000;
-        "window.closeOnFileDelete" = true;
-        "workbench.colorTheme" = "Dark Modern";
-        "workbench.iconTheme" = "vscode-icons";
-        "workbench.preferredDarkColorTheme" = "Dark Modern";
-        "workbench.statusBar.visible" = true;
-        "editor.formatOnSave" = true;
-        "editor.wordWrap" = "on";
-        "editor.wordWrapColumn" = 120;
-        "editor.fontLigatures" = true;
-        "editor.tabSize" = 2;
-
-        # Core Nix support (important for this repo)
-        "nix.enableLanguageServer" = true;
-        "nix.serverPath" = lib.getExe pkgs.nixd;
-        "nix.formatterPath" = lib.getExe pkgs.alejandra;
+      commonSettings = mkCommonSettings {
+        inherit pkgs;
+        inherit lib;
       };
 
-      # Common extensions shared across all profiles
-      commonExtensions =
-        (with pkgs.vscode-extensions; [
-          # Essential Nix support
-          bbenoist.nix
-          jnoortheen.nix-ide
-          kamadorueda.alejandra
-          jeff-hykin.better-nix-syntax
-
-          # Essential workflow
-          mkhl.direnv
-          ms-vscode-remote.remote-ssh
-          christian-kohler.path-intellisense
-        ])
-        ++ (with pkgs.vscode-marketplace; [
-          # Essential UI/UX
-          pkief.material-icon-theme
-          eamodio.gitlens
-          mhutchie.git-graph
-          vivaxy.vscode-conventional-commits
-          aaron-bond.better-comments
-          editorconfig.editorconfig
-          oderwat.indent-rainbow
-          shardulm94.trailing-spaces
-          kisstkondoros.vscode-gutter-preview
-
-          datakurre.devenv
-        ]);
+      commonExtensions = mkCommonExtensions {
+        inherit pkgs;
+        inherit lib;
+      };
     in {
       config = {
         programs.vscode = {
@@ -94,26 +113,25 @@
             # 1. Default Profile: Core configuration + Essential Nix
             default = {
               userSettings = commonSettings;
-              extensions = commonExtensions;
-            };
-
-            # 2. Nix & Shell Dev Profile
-            nix-dev = {
-              userSettings =
-                commonSettings
-                // {
-                  "nix.enableLanguageServer" = true;
-                };
               extensions =
                 commonExtensions
                 ++ (with pkgs.vscode-marketplace; [
                   jnoortheen.xonsh
                   thenuprojectcontributors.vscode-nushell-lang
+
+                  hashicorp.terraform
+                  ms-azuretools.vscode-containers
+                  ms-vscode-remote.remote-containers
+                  github.vscode-github-actions
+                  github.vscode-pull-request-github
+                  gitlab.gitlab-workflow
+
+                  redhat.vscode-yaml
+                  redhat.vscode-xml
                 ]);
             };
 
-            # 3. Web & Frontend Dev Profile
-            web-dev = {
+            web = {
               userSettings = commonSettings;
               extensions =
                 commonExtensions
@@ -149,8 +167,7 @@
                 ]);
             };
 
-            # 4. Data Science & Python Profile
-            data-dev = {
+            python = {
               userSettings =
                 commonSettings
                 // {
@@ -170,8 +187,7 @@
                 ]);
             };
 
-            # 5. Systems & Backend Dev Profile
-            sys-dev = {
+            sysdev = {
               userSettings = commonSettings;
               extensions =
                 commonExtensions
@@ -195,32 +211,10 @@
                   # C/C++
                   llvm-vs-code-extensions.vscode-clangd
                   ms-vscode.makefile-tools
-
-                  # Others
-                  kdl-org.kdl
-                  legale.dts-formatter
                 ]);
             };
 
-            # 6. DevOps & Infrastructure Profile
-            devops = {
-              userSettings = commonSettings;
-              extensions =
-                commonExtensions
-                ++ (with pkgs.vscode-marketplace; [
-                  hashicorp.terraform
-                  ms-azuretools.vscode-containers
-                  ms-vscode-remote.remote-containers
-                  github.vscode-github-actions
-                  github.vscode-pull-request-github
-                  gitlab.gitlab-workflow
-                  redhat.vscode-yaml
-                  redhat.vscode-xml
-                ]);
-            };
-
-            # 7. Mobile Development Profile
-            mobile-dev = {
+            flutter = {
               userSettings = commonSettings;
               extensions =
                 commonExtensions
@@ -230,45 +224,64 @@
                   nash.awesome-flutter-snippets
                 ]);
             };
+          };
+        };
+      };
+    };
 
-            # 8. Godot & C# Profile
-            godot-csharp = {
-              userSettings =
-                commonSettings
-                // {
-                  "csharp.toolsDotnetPath" = "${pkgs.dotnet-sdk_9}/bin/dotnet";
-                  "dotnetAcquisitionExtension.sharedExistingDotnetPath" = "${pkgs.dotnet-sdk_9}/bin/dotnet";
-                  "dotnetAcquisitionExtension.existingDotnetPath" = [
-                    {
-                      "extensionId" = "ms-dotnettools.csharp";
-                      "path" = "${pkgs.dotnet-sdk_9}/bin/dotnet";
-                    }
-                    {
-                      "extensionId" = "ms-dotnettools.csdevkit";
-                      "path" = "${pkgs.dotnet-sdk_9}/bin/dotnet";
-                    }
-                    {
-                      "extensionId" = "woberg.godot-dotnet-tools";
-                      "path" = "${pkgs.dotnet-sdk_8}/bin/dotnet";
-                    }
-                  ];
-                  "godotTools.lsp.serverPort" = 6005;
-                  "omnisharp" = {
-                    "path" = "${pkgs.omnisharp-roslyn}/bin/OmniSharp";
-                    "sdkPath" = "${pkgs.dotnet-sdk_9}";
-                    "dotnetPath" = "${pkgs.dotnet-sdk_9}/bin/dotnet";
-                  };
+    provides.godot-integration = {
+      homeManager = {
+        pkgs,
+        lib,
+        ...
+      }: let
+        commonSettings = mkCommonSettings {
+          inherit pkgs;
+          inherit lib;
+        };
+
+        commonExtensions = mkCommonExtensions {
+          inherit pkgs;
+          inherit lib;
+        };
+      in {
+        programs.vscode.profiles = {
+          godot = {
+            userSettings =
+              commonSettings
+              // {
+                "csharp.toolsDotnetPath" = "${pkgs.dotnet-sdk_9}/bin/dotnet";
+                "dotnetAcquisitionExtension.sharedExistingDotnetPath" = "${pkgs.dotnet-sdk_9}/bin/dotnet";
+                "dotnetAcquisitionExtension.existingDotnetPath" = [
+                  {
+                    "extensionId" = "ms-dotnettools.csharp";
+                    "path" = "${pkgs.dotnet-sdk_9}/bin/dotnet";
+                  }
+                  {
+                    "extensionId" = "ms-dotnettools.csdevkit";
+                    "path" = "${pkgs.dotnet-sdk_9}/bin/dotnet";
+                  }
+                  {
+                    "extensionId" = "woberg.godot-dotnet-tools";
+                    "path" = "${pkgs.dotnet-sdk_8}/bin/dotnet";
+                  }
+                ];
+                "godotTools.lsp.serverPort" = 6005;
+                "omnisharp" = {
+                  "path" = "${pkgs.omnisharp-roslyn}/bin/OmniSharp";
+                  "sdkPath" = "${pkgs.dotnet-sdk_9}";
+                  "dotnetPath" = "${pkgs.dotnet-sdk_9}/bin/dotnet";
                 };
-              extensions =
-                commonExtensions
-                ++ (with pkgs.vscode-extensions; [
-                  geequlim.godot-tools
-                  woberg.godot-dotnet-tools
-                  ms-dotnettools.csdevkit
-                  ms-dotnettools.csharp
-                  ms-dotnettools.vscode-dotnet-runtime
-                ]);
-            };
+              };
+            extensions =
+              commonExtensions
+              ++ (with pkgs.vscode-extensions; [
+                geequlim.godot-tools
+                woberg.godot-dotnet-tools
+                ms-dotnettools.csdevkit
+                ms-dotnettools.csharp
+                ms-dotnettools.vscode-dotnet-runtime
+              ]);
           };
         };
 
