4ef1d902776ce00782437922667ed0ad76dbd2db
loystonpais <loyston500@gmail.com>
2026-04-26 16:45:01 +0000

    browsers: set zen as default browser, add search engines

--- modules/lunar/browsers.nix
+++ modules/lunar/browsers.nix
@@ -22,12 +22,43 @@
           DontCheckDefaultBrowser = true;
           OfferToSaveLogins = false;
         };
+        setAsDefaultBrowser = true;
       };
 
-      programs.chromium = {
-        enable = true;
-        package = pkgs.brave;
+      programs.zen-browser.profiles.default.search = {
+        force = true; # Enforce declared search engines on each rebuild
+        engines = {
+          mynixos = {
+            name = "My NixOS";
+            urls = [
+              {
+                template = "https://mynixos.com/search?q={searchTerms}";
+                params = [
+                  {
+                    name = "query";
+                    value = "searchTerms";
+                  }
+                ];
+              }
+            ];
+            icon = "${pkgs.nixos-icons}/share/icons/hicolor/scalable/apps/nix-snowflake.svg";
+            definedAliases = ["@nx"];
+          };
+          github = {
+            name = "GitHub Search";
+            urls = [
+              {
+                template = "https://github.com/search?q={searchTerms}";
+              }
+            ];
+            definedAliases = ["@gh"];
+          };
+        };
       };
+
+      home.packages = with pkgs; [
+        brave
+      ];
     };
   };
 }
