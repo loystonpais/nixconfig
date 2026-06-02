728bd279d1e4fb74227692b50e360be8df44568b
loystonpais <loyston500@gmail.com>
2026-06-02 14:23:19 +0000

    distrobox: fix: update container_additional_volumes in distrobox configuration

--- modules/lunar/distrobox.nix
+++ modules/lunar/distrobox.nix
@@ -6,7 +6,7 @@
       ];
 
       environment.etc."distrobox/distrobox.conf".text = ''
-        container_additional_volumes="/nix/store:/nix/store:ro /etc/profiles/per-user:/etc/profiles/per-user:ro /etc/static/profiles/per-user:/etc/static/profiles/per-user:ro"
+        container_additional_volumes="/nix:/nix /etc/profiles/per-user:/etc/profiles/per-user:ro /etc/static/profiles/per-user:/etc/static/profiles/per-user:ro /run/current-system/sw:/run/current-system/sw:ro"
       '';
     };
   };
