5a938b3cb5d11aecc2830eb674fb72eefdf70a39
loystonpais <loyston500@gmail.com>
2026-06-11 12:15:51 +0000

    devenv: setup hooks

--- modules/lunar/devenv.nix
+++ modules/lunar/devenv.nix
@@ -15,6 +15,18 @@
           ${lib.getExe pkgs.yq-go} -i ".inputs.nixpkgs.url = \"github:NixOS/nixpkgs/${inputs.nixpkgs.rev}\"" devenv.yaml
         '')
       ];
+
+      programs.zsh.shellInit = ''
+        eval "$(devenv hook zsh)"
+      '';
+
+      programs.bash.shellInit = ''
+        eval "$(devenv hook bash)"
+      '';
+
+      programs.fish.shellInit = ''
+        devenv hook fish | source
+      '';
     };
   };
 }
