f785efb306523f112b4465758d22f0e151fc63ea
loystonpais <loyston500@gmail.com>
2026-06-15 12:30:11 +0530

    cyber: improve config

--- modules/lunar/cyber.nix
+++ modules/lunar/cyber.nix
@@ -4,20 +4,39 @@
   ...
 }: {
   lunar.cyber = {
+    host,
+    user,
+    ...
+  }: {
     nixos = {
       config,
       lib,
       inputs',
+      pkgs-stable,
       ...
     }: {
       imports = [
-        "${inputs.nix-security-box}/wireless.nix"
+        (import "${inputs.nix-security-box}/wireless.nix" {pkgs = pkgs-stable;})
+        (import "${inputs.nix-security-box}/information-gathering.nix" {pkgs = pkgs-stable;})
       ];
 
-      environment.systemPackages = with inputs'.nixpkgs-stable.legacyPackages; [
+      environment.systemPackages = with pkgs-stable; [
         nmap
         masscan
+
+        wireshark
+
+        burpsuite
       ];
+
+      programs.wireshark = {
+        enable = true;
+        dumpcap.enable = true;
+      };
+
+      users.users.${user.userName} = {
+        extraGroups = ["wireshark"];
+      };
     };
   };
 }
