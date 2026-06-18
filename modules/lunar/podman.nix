4ba8ad6d8272506ad36f072137497c53825a8526
loystonpais <loyston500@gmail.com>
2026-06-18 12:31:58 +0000

    podman: remove distrobox script

--- modules/lunar/podman.nix
+++ modules/lunar/podman.nix
@@ -47,12 +47,6 @@
           # Optional: Add your user to the "docker" group to run docker without sudo
           users.users.${user.userName}.extraGroups = ["docker"];
         }
-
-        {
-          environment.etc."distrobox/distrobox.conf".text = ''
-            container_additional_volumes="/nix/store:/nix/store:ro /etc/profiles/per-user:/etc/profiles/per-user:ro /etc/static/profiles/per-user:/etc/static/profiles/per-user:ro"
-          '';
-        }
       ];
     };
   };
