a6c564651b47796dca42d64fbf045542a55568d4
loystonpais <loyston500@gmail.com>
2026-06-19 12:25:39 +0530

    distrobox: failed attempt to fix some issues

--- modules/lunar/distrobox.nix
+++ modules/lunar/distrobox.nix
@@ -3,10 +3,12 @@
     nixos = {pkgs, ...}: {
       environment.systemPackages = with pkgs; [
         distrobox
+        host-spawn
       ];
 
       environment.etc."distrobox/distrobox.conf".text = ''
-        container_additional_volumes="/nix:/nix /etc/profiles/per-user:/etc/profiles/per-user:ro /etc/static/profiles/per-user:/etc/static/profiles/per-user:ro /run/current-system/sw:/run/current-system/sw:ro"
+        container_additional_volumes="/nix/store:/nix/store:ro /etc/profiles/per-user:/etc/profiles/per-user:ro /etc/static/profiles/per-user:/etc/static/profiles/per-user:ro /run/current-system/sw:/run/current-system/sw:ro"
+        container_init_hook="echo 'export PATH=\"/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:\$PATH\"' > /etc/profile.d/fix-nixos-path.sh"
       '';
     };
   };
