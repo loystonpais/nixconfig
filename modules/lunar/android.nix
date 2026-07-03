0bbd4eeb1cb11406050fba989efe4d4e98677e54
loystonpais <loyston500@gmail.com>
2026-07-03 12:18:54 +0530

    android: add adb aliases

--- modules/lunar/android.nix
+++ modules/lunar/android.nix
@@ -4,12 +4,27 @@
     enableScrcpy ? true,
     ...
   }: {
-    nixos = {pkgs, lib, ...}: {
-      environment.systemPackages = with pkgs; [
-        android-tools
-      ] ++ (lib.optionals enableScrcpy [pkgs.scrcpy]);
+    nixos = {
+      pkgs,
+      lib,
+      ...
+    }: {
+      environment.systemPackages = with pkgs;
+        [
+          android-tools
+        ]
+        ++ (lib.optionals enableScrcpy [pkgs.scrcpy]);
 
       users.users.${user.userName}.extraGroups = ["adbusers"];
+    };
+
+    homeManager = {...}: {
+      home.shellAliases = {
+        "adb-call-pick" = "adb shell input keyevent KEYCODE_CALL";
+        "adb-call-end" = "echo adb shell input keyevent KEYCODE_ENDCALL";
+
+        "adb-button-power" = "adb shell input keyevent 26";
+      };
     };
   };
 }
